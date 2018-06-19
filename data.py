12a467a995e041994214e60f082057fc286559fc
Samuel Murray <samuelmu@kth.se>
2018-06-19 19:06:54 +0200

    Add Gaussian blobs

--- data.py
+++ data.py
@@ -13,6 +13,21 @@ def get_circle_data(n_data, output_dim):
     F_true = np.random.multivariate_normal(mean, cov, size=output_dim).T
 
     var_y = 0.01
-    Y = np.random.normal(F_true, var_y)
-    # Y = np.random.poisson(np.exp(F_true))
+    Y = np.empty((n_data, output_dim))
+    mid = output_dim // 2
+    Y[:, :mid] = np.random.normal(F_true[:, :mid], var_y)
+    Y[:, mid:] = np.random.binomial(1, 1 / (1 + np.exp(-F_true[:, mid:])))
+    return Y
+
+
+def get_gaussian_data(n_data):
+    Y = np.empty((n_data, 3))
+    mid = n_data // 2
+    Y[:mid, 0] = np.random.normal(0, .5, size=mid)
+    Y[:mid, 1] = np.random.normal(0, .5, size=mid)
+    Y[:mid, 2] = np.random.binomial(1, 0.7, size=mid)
+
+    Y[mid:, 0] = np.random.normal(1, .5, size=mid)
+    Y[mid:, 1] = np.random.normal(1, .5, size=mid)
+    Y[mid:, 2] = np.random.binomial(1, 0.3, size=mid)
     return Y
