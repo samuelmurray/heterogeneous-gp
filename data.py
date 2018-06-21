ae59bd2d7bf4f57cfc7130fe8d01317650214480
Samuel Murray <samuelmu@kth.se>
2018-06-21 18:05:34 +0200

    Cleaning

--- data.py
+++ data.py
@@ -1,33 +1,34 @@
 import numpy as np
-import GPy
 
 
 def get_circle_data(n_data, output_dim):
+    import GPy
     t = np.linspace(0, (n_data - 1), n_data)
     period = 2 * np.pi / n_data
-    X_true = np.array([np.cos(t * period), np.sin(t * period)]).T
+    x = np.array([np.cos(t * period), np.sin(t * period)]).T
 
-    K_true = GPy.kern.RBF(input_dim=2)
+    k_xx = GPy.kern.RBF(input_dim=2)
     mean = np.zeros(n_data)
-    cov = K_true.K(X_true)
-    F_true = np.random.multivariate_normal(mean, cov, size=output_dim).T
+    cov = k_xx.K(x)
+    f = np.random.multivariate_normal(mean, cov, size=output_dim).T
 
     var_y = 0.01
-    Y = np.empty((n_data, output_dim))
+    y = np.empty((n_data, output_dim))
     mid = output_dim // 2
-    Y[:, :mid] = np.random.normal(F_true[:, :mid], var_y)
-    Y[:, mid:] = np.random.binomial(1, 1 / (1 + np.exp(-F_true[:, mid:])))
-    return Y
+    y[:, :mid] = np.random.normal(f[:, :mid], var_y)
+    y[:, mid:] = np.random.binomial(1, 1 / (1 + np.exp(-f[:, mid:])))
+    return y
 
 
 def get_gaussian_data(n_data):
-    Y = np.empty((n_data, 3))
+    y = np.empty((n_data, 3))
     mid = n_data // 2
-    Y[:mid, 0] = np.random.normal(0, .5, size=mid)
-    Y[:mid, 1] = np.random.normal(0, .5, size=mid)
-    Y[:mid, 2] = np.random.binomial(1, 0.7, size=mid)
 
-    Y[mid:, 0] = np.random.normal(1, .5, size=mid)
-    Y[mid:, 1] = np.random.normal(1, .5, size=mid)
-    Y[mid:, 2] = np.random.binomial(1, 0.3, size=mid)
-    return Y
+    y[:mid, 0] = np.random.normal(0, .5, size=mid)
+    y[:mid, 1] = np.random.normal(0, .5, size=mid)
+    y[:mid, 2] = np.random.binomial(1, 0.7, size=mid)
+
+    y[mid:, 0] = np.random.normal(1, .5, size=mid)
+    y[mid:, 1] = np.random.normal(1, .5, size=mid)
+    y[mid:, 2] = np.random.binomial(1, 0.3, size=mid)
+    return y
