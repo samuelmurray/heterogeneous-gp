fc743fce9730452a64ad1bbeb96bc9cc5d85eb93
Samuel Murray <samuelmu@kth.se>
2018-06-21 18:29:45 +0200

    Remove old code

--- kernel/rbf.py
+++ kernel/rbf.py
@@ -21,12 +21,8 @@ class RBF:
                                  f"but shape(x1)={x1.shape.as_list()} and shape(x2)={x2.shape.as_list()}")
             x1s = tf.reduce_sum(tf.square(x1), axis=-1)
             x2s = tf.reduce_sum(tf.square(_x2), axis=-1)
-
-            # square_dist = -2.0 * tf.matmul(X1, _x2, transpose_b=True) + tf.reshape(x1s, (-1, 1)) + tf.reshape(x2s, (1, -1))
-            # Below is a more general version that should be the same for matrices of rank 2
             square_dist = (-2.0 * tf.matmul(x1, _x2, transpose_b=True)
                            + tf.expand_dims(x1s, axis=-1)
                            + tf.expand_dims(x2s, axis=-2))
-
             rbf = self._variance * tf.exp(-self._gamma * square_dist)
             return (rbf + eps * tf.eye(x1.shape.as_list()[-2])) if x2 is None else rbf
