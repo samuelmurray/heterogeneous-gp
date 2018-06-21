13b15582772c4e1d82f189beb54b87f9e0386650
Samuel Murray <samuelmu@kth.se>
2018-06-21 18:09:23 +0200

    Change kernel parameters to be learnable

--- kernel/rbf.py
+++ kernel/rbf.py
@@ -1,25 +1,32 @@
 import tensorflow as tf
+import numpy as np
 
 
 class RBF:
-    def __init__(self, variance=1, gamma=0.5):
-        self._variance = variance
-        self._gamma = gamma
 
-    def __call__(self, X1, X2=None, *, name="") -> tf.Tensor:
+    def __init__(self, variance=1., gamma=0.5):
+        with tf.variable_scope("kern"):
+            self._log_variance = tf.get_variable("log_variance", 1,
+                                                 initializer=tf.constant_initializer(np.log(variance)))
+            self._variance = tf.exp(self._log_variance, name="variance")
+            self._log_gamma = tf.get_variable("log_gamma", 1, initializer=tf.constant_initializer(np.log(gamma)))
+            self._gamma = tf.exp(self._log_gamma, name="gamma")
+
+    def __call__(self, x1, x2=None, *, name="") -> tf.Tensor:
         with tf.name_scope(name):
             eps = 1e-4
-            _X2 = X1 if X2 is None else X2
-            if X1.shape.as_list()[-1] != _X2.shape.as_list()[-1]:
-                raise ValueError(f"Last dimension of X1 and X2 must match, "
-                                 f"but shape(X1)={X1.shape.as_list()} and shape(X2)={X2.shape.as_list()}")
-            X1s = tf.reduce_sum(tf.square(X1), axis=-1)
-            X2s = tf.reduce_sum(tf.square(_X2), axis=-1)
+            _x2 = x1 if x2 is None else x2
+            if x1.shape.as_list()[-1] != _x2.shape.as_list()[-1]:
+                raise ValueError(f"Last dimension of x1 and x2 must match, "
+                                 f"but shape(x1)={x1.shape.as_list()} and shape(x2)={x2.shape.as_list()}")
+            x1s = tf.reduce_sum(tf.square(x1), axis=-1)
+            x2s = tf.reduce_sum(tf.square(_x2), axis=-1)
 
-            # square_dist = -2.0 * tf.matmul(X1, _X2, transpose_b=True) + tf.reshape(X1s, (-1, 1)) + tf.reshape(X2s, (1, -1))
+            # square_dist = -2.0 * tf.matmul(X1, _x2, transpose_b=True) + tf.reshape(x1s, (-1, 1)) + tf.reshape(x2s, (1, -1))
             # Below is a more general version that should be the same for matrices of rank 2
-            square_dist = -2.0 * tf.matmul(X1, _X2, transpose_b=True) + tf.expand_dims(X1s, axis=-1) + tf.expand_dims(
-                X2s, axis=-2)
+            square_dist = (-2.0 * tf.matmul(x1, _x2, transpose_b=True)
+                           + tf.expand_dims(x1s, axis=-1)
+                           + tf.expand_dims(x2s, axis=-2))
 
             rbf = self._variance * tf.exp(-self._gamma * square_dist)
-            return (rbf + eps * tf.eye(X1.shape.as_list()[-2])) if X2 is None else rbf
+            return (rbf + eps * tf.eye(x1.shape.as_list()[-2])) if x2 is None else rbf
--- run.py
+++ run.py
@@ -3,49 +3,59 @@ import time
 import tensorflow as tf
 import numpy as np
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from IPython import embed
 from sklearn.decomposition import PCA
 
-from data import get_circle_data
+from data import get_circle_data, get_gaussian_data
 from model import MLGPLVM
 import distributions
 
-
 if __name__ == "__main__":
     np.random.seed(1)
-    tf.set_random_seed(1)
+    # tf.set_random_seed(1)
     print("Generating data...")
-    N = 30
-    D = 5
+    N = 100
+    D = 10
     Q = 2
-    y_circle = get_circle_data(N, D)
+    # y_obs = get_circle_data(N, D)
+    y_obs = get_gaussian_data(N)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(y_obs[:N // 2, 0], y_obs[:N // 2, 2], y_obs[:N // 2, 1])
+    ax.scatter(y_obs[N // 2:, 0], y_obs[N // 2:, 2], y_obs[N // 2:, 1])
+    plt.show()
     pca = PCA(Q)
-    x_circle = pca.fit_transform(y_circle)
-    # x_circle = np.random.normal(size=(N, Q))
-    y = tf.convert_to_tensor(y_circle, dtype=tf.float32)
+    # x = pca.fit_transform(y_obs)
+    x = np.random.normal(size=(N, Q))
+    y = tf.convert_to_tensor(y_obs, dtype=tf.float32)
 
     print("Creating model...")
-    dist_list = [distributions.normal for _ in range(D)]
-    m = MLGPLVM(y, x_circle, dist_list)
+    # dist_list = [distributions.normal for _ in range(D // 2)] + [distributions.bernoulli for _ in range(D // 2)]
+    # dist_list = [distributions.normal for _ in range(D)]
+    dist_list = [distributions.normal, distributions.normal, distributions.bernoulli]
+    # dist_list = [distributions.normal, distributions.normal, distributions.normal]
+    m = MLGPLVM(y, x, dist_list)
 
     print("Building graph...")
     loss = m.loss()
 
-    learning_rate = 1e-4
+    learning_rate = 5e-4
     with tf.name_scope("train"):
         with tf.variable_scope("", reuse=tf.AUTO_REUSE):
-            train_x = tf.train.RMSPropOptimizer(learning_rate).minimize(
-                loss, var_list=[tf.get_variable("z"), tf.get_variable("qx/mean"), tf.get_variable("qx/log_std")],
-                name="train_x")
-            train_u = tf.train.RMSPropOptimizer(learning_rate).minimize(
-                loss, var_list=[tf.get_variable("qu/mean"), tf.get_variable("qu/log_scale")],
-                name="train_u")
+            u_vars = [tf.get_variable("qu/mean"), tf.get_variable("qu/log_scale")]
+            non_u_vars = [tf.get_variable("z"), tf.get_variable("qx/mean"), tf.get_variable("qx/log_std"),
+                          tf.get_variable("kern/log_variance"), tf.get_variable("kern/log_gamma")]
+            train_x = tf.train.RMSPropOptimizer(learning_rate).minimize(loss, var_list=non_u_vars, name="train_x")
+            train_u = tf.train.RMSPropOptimizer(learning_rate).minimize(loss, var_list=u_vars, name="train_u")
 
     with tf.name_scope("summary"):
         tf.summary.scalar("kl_qx_px", m.kl_qx_px(), collections=["training"])
         tf.summary.scalar("kl_qu_pu", m.kl_qu_pu(), collections=["training"])
         tf.summary.scalar("expectation", m.mc_expectation(), collections=["training"])
         tf.summary.scalar("training_loss", loss, collections=["training"])
+        tf.summary.scalar("kern_var", tf.squeeze(m.kern._variance), collections=["training"])
+        tf.summary.scalar("kern_gamma", tf.squeeze(m.kern._gamma), collections=["training"])
         tf.summary.histogram("qx_mean", m.qx_mean, collections=["training"])
         tf.summary.histogram("qx_std", m.qx_std, collections=["training"])
         tf.summary.histogram("z", m.z, collections=["training"])
@@ -78,8 +88,9 @@ if __name__ == "__main__":
                 print(loss_print)
                 x_mean = sess.run(m.qx_mean)
                 z = sess.run(m.z)
-                plt.scatter(x_mean[:, 0], x_mean[:, 1], c="b")
-                plt.plot(x_mean[:, 0], x_mean[:, 1], c="b")
+                plt.scatter(x_mean[:N // 2, 0], x_mean[:N // 2, 1])  # , c="b")
+                plt.scatter(x_mean[N // 2:, 0], x_mean[N // 2:, 1])  # , c="b")
+                # plt.plot(x_mean[:, 0], x_mean[:, 1], c="b")
                 # plt.scatter(z[:, 0], z[:, 1], c="k", marker="x")
                 plt.title(loss_print)
                 plt.pause(0.05)
