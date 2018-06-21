c31b856c53a06ef55fee330c12fc08ff1ecf368a
Samuel Murray <samuelmu@kth.se>
2018-06-21 18:08:54 +0200

    PEP8ing much

--- model/mlgplvm.py
+++ model/mlgplvm.py
@@ -7,18 +7,20 @@ from kernel import RBF
 
 
 class MLGPLVM:
+
     def __init__(self, y: tf.Tensor, x: np.ndarray,
-                 distribution_list: List[Callable[[tf.Tensor], tf.distributions.Distribution]]):
+                 likelihoods: List[Callable[[tf.Tensor], tf.distributions.Distribution]]):
         if x.shape[0] != y.shape.as_list()[0]:
             raise ValueError(
                 f"First dimension of x and y must match, but shape(x)={list(x.shape)} and shape(y)={y.shape.as_list()}")
-        if len(distribution_list) != y.shape.as_list()[1]:
+        if len(likelihoods) != y.shape.as_list()[1]:
             raise ValueError(
-                f"Must provide one distribution per y dimension, but len(distribtution_list)={len(distribution_list)} and shape(y)={y.shape.as_list()}")
-        self.rbf = RBF()
-        self._distributions_list = distribution_list
+                f"Must provide one distribution per y dimension, "
+                f"but len(likelihoods)={len(likelihoods)} and shape(y)={y.shape.as_list()}")
+        self.kern = RBF()
+        self._likelihoods = likelihoods
         self._latent_dim = x.shape[1]
-        self.num_inducing = 20
+        self.num_inducing = 50
         self.y = y
 
         with tf.variable_scope("qx"):
@@ -52,14 +54,14 @@ class MLGPLVM:
         with tf.name_scope("kl_qx_px"):
             qx = tf.distributions.Normal(self.qx_mean, self.qx_std, name="qx")
             px = tf.distributions.Normal(0., 1., name="px")
-            kl = tf.reduce_sum(tf.distributions.kl_divergence(qx, px, allow_nan_stats=False), axis=[0, 1], name="kl")
+            kl = tf.reduce_sum(tf.distributions.kl_divergence(qx, px, ajjllow_nan_stats=False), axis=[0, 1], name="kl")
         return kl
 
     def kl_qu_pu(self):
         with tf.name_scope("kl_qu_pu"):
             # TODO: Figure out why nodes pu_2, qu_2, normal and normal_2 are created. Done by MultivariateNormalTriL?
             qu = tf.contrib.distributions.MultivariateNormalTriL(self.qu_mean, self.qu_scale, name="qu")
-            k_zz = self.rbf(self.z, name="k_zz")
+            k_zz = self.kern(self.z, name="k_zz")
             l_zz = tf.tile(tf.expand_dims(tf.cholesky(k_zz), axis=0), [self.ydim, 1, 1], name="l_zz")
             pu = tf.contrib.distributions.MultivariateNormalTriL(tf.zeros([self.ydim, self.num_inducing]), l_zz,
                                                                  name="pu")
@@ -77,13 +79,13 @@ class MLGPLVM:
 
     def log_prob(self, f):
         with tf.name_scope("log_prob"):
-            log_prob = tf.stack([self._distributions_list[i](f[:, i, :]).log_prob(tf.transpose(self.y[:, i])) for i in
-                                 range(self.ydim)], axis=1)
+            log_prob = tf.stack([self._likelihoods[i](f[:, i, :]).log_prob(tf.transpose(self.y[:, i]))
+                                 for i in range(self.ydim)], axis=1)
             return log_prob
 
     def sample_f(self, num_samples: int):
         with tf.name_scope("sample_f"):
-            k_zz = self.rbf(self.z, name="k_zz")
+            k_zz = self.kern(self.z, name="k_zz")
             k_zz_inv = tf.matrix_inverse(k_zz, name="k_zz_inv")
 
             # x = qx_mean + qx_std * e_x, e_x ~ N(0,1)
@@ -96,10 +98,10 @@ class MLGPLVM:
             u_sample = tf.add(self.qu_mean, tf.einsum("ijk,tik->tij", self.qu_scale, e_u), name="u_sample")
             assert u_sample.shape.as_list() == [num_samples, self.ydim, self.num_inducing]
 
-            k_zx = self.rbf(tf.tile(tf.expand_dims(self.z, axis=0), multiples=[num_samples, 1, 1]), x_sample,
-                            name="k_zx")
+            k_zx = self.kern(tf.tile(tf.expand_dims(self.z, axis=0), multiples=[num_samples, 1, 1]), x_sample,
+                             name="k_zx")
             assert k_zx.shape.as_list() == [num_samples, self.num_inducing, self.num_data]
-            k_xx = self.rbf(x_sample, name="k_xx")
+            k_xx = self.kern(x_sample, name="k_xx")
             assert k_xx.shape.as_list() == [num_samples, self.num_data, self.num_data]
 
             a = tf.einsum("ij,sjk->sik", k_zz_inv, k_zx, name="a")
@@ -112,7 +114,8 @@ class MLGPLVM:
 
             # f = a.T * u + sqrt(b) * e_f, e_f ~ N(0,1)
             e_f = tf.random_normal(shape=[num_samples, self.ydim, self.num_data], name="e_f")
-            f_samples = tf.add(tf.matmul(u_sample, a), tf.multiply(tf.expand_dims(tf.sqrt(b), 1), e_f),
+            f_samples = tf.add(tf.matmul(u_sample, a),
+                               tf.multiply(tf.expand_dims(tf.sqrt(b), 1), e_f),
                                name="f_samples")
             return f_samples
 
