ec904bf48570ac121658420f0c850b0efc449c3d
Samuel Murray <samuelmu@kth.se>
2018-06-25 11:21:17 +0200

    Move run script to separate file

---
+++ examples/run_gplvm.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from matplotlib import pyplot as plt
+from IPython import embed
+
+from util.data import get_circle_data
+from model import GPLVM
+
+if __name__ == "__main__":
+    y_train, _ = get_circle_data(50, 10)
+    y = tf.convert_to_tensor(y_train, dtype=tf.float32)
+
+    gplvm = GPLVM(y, 2)
+
+    loss = -gplvm.log_joint()
+    learning_rate = 0.1
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+    train = optimizer.minimize(loss)
+    init = tf.global_variables_initializer()
+    plt.axis([-5, 5, -5, 5])
+    plt.ion()
+    with tf.Session() as sess:
+        sess.run(init)
+        n_iter = 2000
+        for i in range(n_iter):
+            sess.run(train)
+            if i % 100 == 0:
+                print(f"Step {i} - Log joint: {sess.run(gplvm.log_joint())}")
+                x = sess.run(gplvm.x)
+                plt.plot(*x.T)
+                plt.scatter(*x.T)
+                plt.pause(0.05)
+                plt.cla()
+        x = sess.run(gplvm.x)
+        plt.plot(*x.T)
+        plt.scatter(*x.T)
+        embed()
