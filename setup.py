e009bd64b1b1a9ab5db7bc49f0781a3571fc06b6
Samuel Murray <samuelmu@kth.se>
2018-06-26 15:38:35 +0200

    Added pip support

---
+++ setup.py
@@ -0,0 +1,46 @@
+import os
+
+from setuptools import setup, find_packages
+
+NAME = "gp"
+DESCRIPTION = "MLGPLVM implementation in TF"
+URL = "https://github.com/samuelmurray/TF-GP"
+EMAIL = "samuelmu@kth.se"
+AUTHOR = "Samuel Murray"
+REQUIRES_PYTHON = ">=3.6.0"
+LICENSE = "GNU General Public License v3.0"
+
+REQUIRED = [
+    "numpy",
+    "tensorflow",
+    "matplotlib",
+]
+
+# Read version number
+version_dummy = {}
+with open(os.path.join(NAME, '__version__.py')) as f:
+    exec(f.read(), version_dummy)
+VERSION = version_dummy["__version__"]
+del version_dummy
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license=LICENSE,
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
