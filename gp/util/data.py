d5b7f733c1deafb5ecf6b082abdab81d6c2bedb9
Samuel Murray <samuelmu@kth.se>
2018-06-27 12:56:46 +0200

    All methods return labels

--- gp/util/data.py
+++ gp/util/data.py
@@ -9,7 +9,7 @@ from . import distributions
 Likelihood = Callable[[tf.Tensor], ds.Distribution]
 
 
-def get_circle_data(n_data: int, output_dim: int, gaussian: bool = True) -> Tuple[np.ndarray, List[Likelihood]]:
+def get_circle_data(n_data: int, output_dim: int, gaussian: bool = True) -> Tuple[np.ndarray, List[Likelihood], np.ndarray]:
     import GPy
     t = np.linspace(0, (n_data - 1), n_data)
     period = 2 * np.pi / n_data
@@ -29,23 +29,28 @@ def get_circle_data(n_data: int, output_dim: int, gaussian: bool = True) -> Tupl
         mid = output_dim // 2
         y[:, :mid] = np.random.normal(f[:, :mid], var_y)
         y[:, mid:] = np.random.binomial(1, 1 / (1 + np.exp(-f[:, mid:])))
-        likelihoods = [distributions.normal for _ in range(mid)] + [distributions.bernoulli for _ in range(mid)]
-    return y, likelihoods
+        likelihoods = [distributions.normal for _ in range(mid)] + [distributions.bernoulli for _ in range(n_data - mid)]
+    labels = np.zeros(n_data)
+    return y, likelihoods, labels
 
 
-def get_gaussian_data(n_data: int) -> Tuple[np.ndarray, List[Likelihood]]:
+def get_gaussian_data(n_data: int) -> Tuple[np.ndarray, List[Likelihood], np.ndarray]:
     y = np.empty((n_data, 3))
+    labels = np.empty(n_data)
     mid = n_data // 2
 
     y[:mid, 0] = np.random.normal(0, .5, size=mid)
     y[:mid, 1] = np.random.normal(0, .5, size=mid)
     y[:mid, 2] = np.random.binomial(1, 0.7, size=mid)
+    labels[:mid] = np.zeros(mid)
 
-    y[mid:, 0] = np.random.normal(1, .5, size=mid)
-    y[mid:, 1] = np.random.normal(1, .5, size=mid)
-    y[mid:, 2] = np.random.binomial(1, 0.3, size=mid)
+    y[mid:, 0] = np.random.normal(1, .5, size=n_data - mid)
+    y[mid:, 1] = np.random.normal(1, .5, size=n_data - mid)
+    y[mid:, 2] = np.random.binomial(1, 0.3, size=n_data - mid)
+    labels[mid:] = np.ones(n_data - mid)
+
     likelihoods = [distributions.normal, distributions.normal, distributions.bernoulli]
-    return y, likelihoods
+    return y, likelihoods, labels
 
 
 def oilflow(n_data: int = None, *, one_hot_labels: bool = False) -> Tuple[np.ndarray, List[Likelihood], np.ndarray]:
