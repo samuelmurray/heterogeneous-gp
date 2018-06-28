0d1cb6d796a16a1a10f65fc91be41fac064c8906
Samuel Murray <samuelmu@kth.se>
2018-06-28 13:43:15 +0200

    Add ARD RBF kernel

--- gp/kernel/__init__.py
+++ gp/kernel/__init__.py
@@ -3,3 +3,4 @@ Module for all supported kernels
 """
 
 from .rbf import RBF
+from .ard_rbf import ARDRBF
---
+++ gp/kernel/ard_rbf.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import numpy as np
+
+from .kernel import Kernel
+
+
+class ARDRBF(Kernel):
+    def __init__(self, variance: float = 1., gamma: float = 0.5, *, xdim: int, eps: float = 1e-4) -> None:
+        super().__init__()
+        with tf.variable_scope("kern"):
+            self._xdim = xdim
+            self._log_variance = tf.get_variable("log_variance", shape=[1],
+                                                 initializer=tf.constant_initializer(np.log(variance)))
+            self._variance = tf.exp(self._log_variance, name="variance")
+            self._log_gamma = tf.get_variable("log_gamma", shape=[xdim],
+                                              initializer=tf.constant_initializer(np.log(gamma)))
+            self._gamma = tf.exp(self._log_gamma, name="gamma")
+            self._eps = eps
+
+    def __call__(self, x1: tf.Tensor, x2: tf.Tensor = None, *, name: str = "") -> tf.Tensor:
+        with tf.name_scope(name):
+            _x2 = x1 if x2 is None else x2
+            for _x in [x1, _x2]:
+                if _x.shape.as_list()[-1] != self._xdim:
+                    raise ValueError(f"Last dimension of input must be {self._xdim}, but shape(x)={_x.shape.as_list()}")
+            scaled_x1 = tf.multiply(x1, tf.sqrt(self._gamma))
+            scaled_x2 = tf.multiply(_x2, tf.sqrt(self._gamma))
+            x1_squared = tf.reduce_sum(tf.square(scaled_x1), axis=-1)
+            x2_squared = tf.reduce_sum(tf.square(scaled_x2), axis=-1)
+            square_dist = (-2.0 * tf.matmul(scaled_x1, scaled_x2, transpose_b=True)
+                           + tf.expand_dims(x1_squared, axis=-1)
+                           + tf.expand_dims(x2_squared, axis=-2))
+            rbf = self._variance * tf.exp(-square_dist)
+            return (rbf + self._eps * tf.eye(x1.shape.as_list()[-2])) if x2 is None else rbf
