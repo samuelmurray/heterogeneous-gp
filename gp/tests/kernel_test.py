5a0df1b43279a748a71d9f2ff6a1dff86cc6b139
Samuel Murray <samuelmu@kth.se>
2018-06-28 13:48:40 +0200

    Tests for ARD kernel

--- gp/tests/kernel_test.py
+++ gp/tests/kernel_test.py
@@ -1,26 +1,90 @@
 import tensorflow as tf
 import numpy as np
 
-from gp.kernel import RBF
+from sklearn.metrics.pairwise import rbf_kernel
+from gp.kernel import RBF, ARDRBF
 
 
 class TestRBF(tf.test.TestCase):
 
     def setUp(self):
-        with tf.variable_scope("", reuse=tf.AUTO_REUSE):
+        with tf.variable_scope("rbf", reuse=tf.AUTO_REUSE):
             self.kern = RBF(1., 0.5)
 
-    def test_RBF_simple(self):
+    def test_throw(self):
+        a = tf.convert_to_tensor(np.random.normal(size=(10, 5)), dtype=tf.float32)
+        b = tf.convert_to_tensor(np.random.normal(size=(12, 4)), dtype=tf.float32)
+        exception_thrown = False
+        try:
+            _ = self.kern(a, b)
+        except ValueError:
+            exception_thrown = True
+        self.assertTrue(exception_thrown)
+
+    def test_simple(self):
         a = tf.convert_to_tensor(np.random.normal(size=(10, 5)), dtype=tf.float32)
         k = self.kern(a)
         self.assertShapeEqual(np.empty([10, 10]), k)
 
-    def test_RBF_dimensions(self):
+    def test_extended(self):
         a = tf.convert_to_tensor(np.random.normal(size=(2, 10, 5)), dtype=tf.float32)
         k = self.kern(a)
         self.assertShapeEqual(np.empty([2, 10, 10]), k)
 
-    def test_RBF_full(self):
+    def test_full(self):
+        a = tf.convert_to_tensor(np.random.normal(size=(2, 10, 5)), dtype=tf.float32)
+        b = tf.convert_to_tensor(np.random.normal(size=(2, 12, 5)), dtype=tf.float32)
+        k = self.kern(a, b)
+        self.assertShapeEqual(np.empty([2, 10, 12]), k)
+
+    def test_equal_to_sklearn(self):
+        a = np.random.normal(size=(5, 5))
+        b = np.random.normal(size=(6, 5))
+        k_sklearn = rbf_kernel(a, b, gamma=0.5)
+        k_rbf = self.kern(tf.convert_to_tensor(a, dtype=tf.float32), tf.convert_to_tensor(b, dtype=tf.float32))
+        init = tf.global_variables_initializer()
+        with self.test_session() as sess:
+            sess.run(init)
+            self.assertAllClose(k_rbf.eval(), k_sklearn)
+
+
+class TestARDRBF(tf.test.TestCase):
+
+    def setUp(self):
+        with tf.variable_scope("ardrbf", reuse=tf.AUTO_REUSE):
+            self.kern = ARDRBF(1., 0.5, xdim=5)
+
+    def test_throw(self):
+        a = tf.convert_to_tensor(np.random.normal(size=(10, 5)), dtype=tf.float32)
+        b = tf.convert_to_tensor(np.random.normal(size=(12, 4)), dtype=tf.float32)
+        exception_thrown = False
+        try:
+            _ = self.kern(a, b)
+        except ValueError:
+            exception_thrown = True
+        self.assertTrue(exception_thrown)
+
+    def test_equal_to_sklearn(self):
+        a = np.random.normal(size=(7, 5))
+        b = np.random.normal(size=(6, 5))
+        k_sklearn = rbf_kernel(a, b, gamma=0.5)
+        k_ard_rbf = self.kern(tf.convert_to_tensor(a, dtype=tf.float32), tf.convert_to_tensor(b, dtype=tf.float32))
+        init = tf.global_variables_initializer()
+        with self.test_session() as sess:
+            sess.run(init)
+            self.assertAllClose(k_ard_rbf.eval(), k_sklearn)
+
+    def test_simple(self):
+        a = tf.convert_to_tensor(np.random.normal(size=(10, 5)), dtype=tf.float32)
+        k = self.kern(a)
+        self.assertShapeEqual(np.empty([10, 10]), k)
+
+    def test_extended(self):
+        a = tf.convert_to_tensor(np.random.normal(size=(2, 10, 5)), dtype=tf.float32)
+        k = self.kern(a)
+        self.assertShapeEqual(np.empty([2, 10, 10]), k)
+
+    def test_full(self):
         a = tf.convert_to_tensor(np.random.normal(size=(2, 10, 5)), dtype=tf.float32)
         b = tf.convert_to_tensor(np.random.normal(size=(2, 12, 5)), dtype=tf.float32)
         k = self.kern(a, b)
