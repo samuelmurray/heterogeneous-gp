91f4d59cc20bdc730a99eeeaa22e15f663dcbd7e
Samuel Murray <samuelmu@kth.se>
2018-06-29 18:23:31 +0200

    Make likelihoods classes, to enable trainable parameters

--- gp/util/data.py
+++ gp/util/data.py
@@ -1,18 +1,14 @@
-from typing import Tuple, List, Callable
+from typing import Tuple, List
 
-import tensorflow as tf
-import tensorflow.contrib.distributions as ds
 import numpy as np
 from sklearn.metrics.pairwise import rbf_kernel
 import pods
 
 from . import distributions
 
-Likelihood = Callable[[tf.Tensor], ds.Distribution]
-
 
 def circle_data(num_data: int, output_dim: int,
-                gaussian: bool = True) -> Tuple[np.ndarray, List[Likelihood], np.ndarray]:
+                gaussian: bool = True) -> Tuple[np.ndarray, List[distributions.Likelihood], np.ndarray]:
     t = np.linspace(0, 2 * np.pi, num_data, endpoint=False)
     x = np.array([np.cos(t), np.sin(t)]).T
     mean = np.zeros(num_data)
@@ -23,18 +19,18 @@ def circle_data(num_data: int, output_dim: int,
     y = np.empty((num_data, output_dim))
     if gaussian:
         y = np.random.normal(f, var_y)
-        likelihoods = [distributions.normal for _ in range(output_dim)]
+        likelihoods = [distributions.Normal() for _ in range(output_dim)]
     else:
         half_output = output_dim // 2
         y[:, :half_output] = np.random.normal(f[:, :half_output], var_y)
         y[:, half_output:] = np.random.binomial(1, 1 / (1 + np.exp(-f[:, half_output:])))
-        likelihoods = ([distributions.normal for _ in range(half_output)] +
-                       [distributions.bernoulli for _ in range(output_dim - half_output)])
+        likelihoods = ([distributions.Normal() for _ in range(half_output)] +
+                       [distributions.Bernoulli() for _ in range(output_dim - half_output)])
     labels = np.zeros(num_data)
     return y, likelihoods, labels
 
 
-def gaussian_data(num_data: int) -> Tuple[np.ndarray, List[Likelihood], np.ndarray]:
+def gaussian_data(num_data: int) -> Tuple[np.ndarray, List[distributions.Likelihood], np.ndarray]:
     y = np.empty((num_data, 3))
     labels = np.empty(num_data)
     half_data = num_data // 2
@@ -48,16 +44,17 @@ def gaussian_data(num_data: int) -> Tuple[np.ndarray, List[Likelihood], np.ndarr
     y[half_data:, 2] = np.random.binomial(1, 0.3, size=num_data - half_data)
     labels[half_data:] = np.ones(num_data - half_data)
 
-    likelihoods = [distributions.normal, distributions.normal, distributions.bernoulli]
+    likelihoods = [distributions.Normal(), distributions.Normal(), distributions.Bernoulli()]
     return y, likelihoods, labels
 
 
-def oilflow(num_data: int = None, *, one_hot_labels: bool = False) -> Tuple[np.ndarray, List[Likelihood], np.ndarray]:
+def oilflow(num_data: int = None, *,
+            one_hot_labels: bool = False) -> Tuple[np.ndarray, List[distributions.Likelihood], np.ndarray]:
     oil = pods.datasets.oil()
     indices = np.random.permutation(1000)[:num_data]
     y = oil['X'][indices, :]
     labels = oil['Y'][indices, :]
-    likelihoods = [distributions.normal for _ in range(y.shape[1])]
+    likelihoods = [distributions.Normal() for _ in range(y.shape[1])]
     if not one_hot_labels:
         labels = np.argmax(labels, axis=1)
     return y, likelihoods, labels
--- gp/util/distributions.py
+++ gp/util/distributions.py
@@ -1,13 +1,51 @@
 import tensorflow as tf
+import numpy as np
 
 
-def normal(f: tf.Tensor) -> tf.distributions.Normal:
-    return tf.distributions.Normal(loc=f, scale=1.)
+class Likelihood:
+    def __init__(self):
+        self._summary_family = "Likelihood"
+
+    def __call__(self, f: tf.Tensor) -> tf.distributions.Distribution:
+        raise NotImplementedError
+
+    def create_summaries(self) -> None:
+        raise NotImplementedError
 
 
-def bernoulli(f: tf.Tensor) -> tf.distributions.Bernoulli:
-    return tf.distributions.Bernoulli(logits=f)
+class Normal(Likelihood):
+    __count = 0
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._id = self.__get_id()
+        self._log_scale = tf.get_variable(f"normal_log_scale_{self._id}", shape=[1],
+                                          initializer=tf.constant_initializer(np.log(0.1)))
+        self._scale = tf.exp(self._log_scale, name=f"normal_scale_{self._id}")
+
+    def __call__(self, f: tf.Tensor) -> tf.distributions.Normal:
+        return tf.distributions.Normal(loc=f, scale=self._scale)
+
+    def create_summaries(self) -> None:
+        tf.summary.scalar(f"normal_scale_{self._id}", tf.squeeze(self._scale), family=self._summary_family)
+
+    @classmethod
+    def __get_id(cls) -> int:
+        cls.__count += 1
+        return cls.__count
 
 
-def poisson(f: tf.Tensor) -> tf.contrib.distributions.Poisson:
-    return tf.contrib.distributions.Poisson(log_rate=f)
+class Bernoulli(Likelihood):
+    def __call__(self, f: tf.Tensor) -> tf.distributions.Bernoulli:
+        return tf.distributions.Bernoulli(logits=f)
+
+    def create_summaries(self) -> None:
+        pass
+
+
+class Poisson(Likelihood):
+    def __call__(self, f: tf.Tensor) -> tf.contrib.distributions.Poisson:
+        return tf.contrib.distributions.Poisson(log_rate=f)
+
+    def create_summaries(self) -> None:
+        pass
