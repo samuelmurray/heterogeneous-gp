6642721786c13833ea18fdf41a1d923272bee819
Samuel Murray <samuelmu@kth.se>
2018-07-02 13:09:57 +0200

    Change root name to avoid name conflict

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-NAME = "gp"
+NAME = "tf-gp"
 DESCRIPTION = "MLGPLVM implementation in TF"
 URL = "https://github.com/samuelmurray/TF-GP"
 EMAIL = "samuelmu@kth.se"
