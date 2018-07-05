53b0c2ab98507f6f100d31f75223d20592a0a6ac
Samuel Murray <samuelmu@kth.se>
2018-07-05 18:13:35 +0200

    Group nodes in tensorboard

--- tfgp/util/distributions.py
+++ tfgp/util/distributions.py
@@ -19,8 +19,9 @@ class Normal(Likelihood):
     def __init__(self) -> None:
         super().__init__()
         self._id = self.__get_id()
-        self._log_scale = tf.get_variable(f"normal_log_scale_{self._id}", shape=[1],
-                                          initializer=tf.constant_initializer(np.log(0.1)))
+        with tf.variable_scope("likelihood"):
+            self._log_scale = tf.get_variable(f"normal_log_scale_{self._id}", shape=[1],
+                                              initializer=tf.constant_initializer(np.log(0.1)))
         self._scale = tf.exp(self._log_scale, name=f"normal_scale_{self._id}")
 
     def __call__(self, f: tf.Tensor) -> tf.distributions.Normal:
