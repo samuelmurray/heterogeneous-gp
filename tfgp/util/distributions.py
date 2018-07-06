9bb633de7494289548d294e406e42530eeb42118
Samuel Murray <samuelmu@kth.se>
2018-07-06 16:11:35 +0200

    Group all likelihood parameters

--- tfgp/util/distributions.py
+++ tfgp/util/distributions.py
@@ -22,7 +22,7 @@ class Normal(Likelihood):
         with tf.variable_scope("likelihood"):
             self._log_scale = tf.get_variable(f"normal_log_scale_{self._id}", shape=[1],
                                               initializer=tf.constant_initializer(np.log(0.1)))
-        self._scale = tf.exp(self._log_scale, name=f"normal_scale_{self._id}")
+        self._scale = tf.exp(self._log_scale, name=f"likelihood/normal_scale_{self._id}")
 
     def __call__(self, f: tf.Tensor) -> tf.distributions.Normal:
         return tf.distributions.Normal(loc=f, scale=self._scale)
