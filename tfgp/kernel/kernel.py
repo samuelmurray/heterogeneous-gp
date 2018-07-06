fb458b98b9e5ff1ac230d4ea089b1d3132e39158
Samuel Murray <samuelmu@kth.se>
2018-07-06 16:12:50 +0200

    Minor

--- tfgp/kernel/kernel.py
+++ tfgp/kernel/kernel.py
@@ -5,7 +5,6 @@ class Kernel:
     def __init__(self, name: str) -> None:
         self._summary_family = "Kernel"
         self._name = name
-        pass
 
     def __call__(self, x1: tf.Tensor, x2: tf.Tensor = None, *, name: str = "") -> tf.Tensor:
         raise NotImplementedError
