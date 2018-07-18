cf2da56327c9f742fbe84cf4cfa7ba0cb377df28
Samuel Murray <samuelmu@kth.se>
2018-07-18 15:38:58 +0200

    Make base classes abstract

--- tfgp/kernel/kernel.py
+++ tfgp/kernel/kernel.py
@@ -1,13 +1,17 @@
+import abc
+
 import tensorflow as tf
 
 
-class Kernel:
+class Kernel(abc.ABC):
     def __init__(self, name: str) -> None:
         self._summary_family = "Kernel"
         self._name = name
 
+    @abc.abstractmethod
     def __call__(self, x1: tf.Tensor, x2: tf.Tensor = None, *, name: str = "") -> tf.Tensor:
         raise NotImplementedError
 
+    @abc.abstractmethod
     def create_summaries(self) -> None:
         raise NotImplementedError
