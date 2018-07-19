d8b0c82bac410e4c17733e45482f06014effc0ff
Samuel Murray <samuelmu@kth.se>
2018-07-19 14:03:12 +0200

    Inital commit

---
+++ tfgp/examples/run_mlgp.py
@@ -0,0 +1,72 @@
+import time
+
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+
+from tfgp import likelihood
+from tfgp.model import MLGP
+
+from IPython import embed
+
+if __name__ == "__main__":
+    print("Generating data...")
+    x = np.linspace(0, 2 * np.pi, 20)[:, None]
+    y = np.sin(x)
+    likelihoods = [likelihood.Normal()]
+    num_inducing = 10
+
+    print("Creating model...")
+    m = MLGP(x, y, likelihoods=likelihoods, num_inducing=num_inducing)
+
+    print("Building graph...")
+    loss = tf.losses.get_total_loss()
+    learning_rate = 5e-4
+    with tf.name_scope("train"):
+        optimizer = tf.train.RMSPropOptimizer(learning_rate, name="RMSProp")
+        train_all = optimizer.minimize(loss, var_list=tf.trainable_variables(),
+                                       global_step=tf.train.create_global_step(),
+                                       name="train")
+    with tf.name_scope("summary"):
+        m.create_summaries()
+        tf.summary.scalar("total_loss", loss, family="Loss")
+        for reg_loss in tf.losses.get_regularization_losses():
+            tf.summary.scalar(f"{reg_loss.name}", reg_loss, family="Loss")
+        merged_summary = tf.summary.merge_all()
+
+    init = tf.global_variables_initializer()
+
+    plt.axis([-5, 5, -5, 5])
+    plt.ion()
+    x_test = np.linspace(-2, 2 * np.pi + 2, 30)[:, None]
+    with tf.Session() as sess:
+        log_dir = f"../../log/mlgp/{time.strftime('%Y%m%d%H%M%S')}"
+        summary_writer = tf.summary.FileWriter(log_dir, sess.graph)
+        print("Initializing variables...")
+        sess.run(init)
+        print(f"Initial loss: {sess.run(loss)}")
+        print("Starting training...")
+        n_iter = 50000
+        n_print = 300
+        for i in range(n_iter):
+            sess.run(train_all)
+            if i % n_print == 0:
+                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                run_metadata = tf.RunMetadata()
+                train_loss, summary = sess.run([loss, merged_summary], options=run_options, run_metadata=run_metadata)
+                summary_writer.add_run_metadata(run_metadata, f"step{i}")
+                summary_writer.add_summary(summary, i)
+                loss_print = f"Step {i} - Loss: {train_loss}"
+                print(loss_print)
+                z = sess.run(m.z)
+                u = sess.run(m.qu_mean)
+                y_pred = sess.run(m.predict(x_test))
+                plt.scatter(x, y, marker="o")
+                plt.scatter(z, u, c="k", marker="x")
+                plt.plot(x_test, y_pred)
+                plt.title(loss_print)
+                plt.pause(0.05)
+                plt.cla()
+        z = sess.run(m.z)
+        plt.scatter(*z.T, c="k", marker="x")
+        embed()
