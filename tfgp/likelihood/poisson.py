60c4ae89ea79e64aad6fffa8f616d37727115344
Samuel Murray <samuelmu@kth.se>
2018-07-20 16:34:10 +0200

    Make __init__ explicit

--- tfgp/likelihood/poisson.py
+++ tfgp/likelihood/poisson.py
@@ -4,6 +4,9 @@ from . import Likelihood
 
 
 class Poisson(Likelihood):
+    def __init__(self) -> None:
+        super().__init__()
+
     def __call__(self, f: tf.Tensor) -> tf.contrib.distributions.Poisson:
         return tf.contrib.distributions.Poisson(log_rate=f)
 
