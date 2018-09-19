c0a6e97ea6647cb2cba5e29cd2721f9c7ff5aed8
Samuel Murray <samuelmu@kth.se>
2018-09-19 18:23:56 +0200

    MLGPLVM inherits from MLGP to remove duplicate code

--- tfgp/model/mlgplvm.py
+++ tfgp/model/mlgplvm.py
@@ -1,40 +1,26 @@
-from typing import Tuple
-
 import tensorflow as tf
 import tensorflow_probability as tfp
 import numpy as np
 
-from .inducing_points_model import InducingPointsModel
+from .mlgp import MLGP
 from ..kernel import Kernel
-from ..kernel import RBF
 from ..likelihood import MixedLikelihoodWrapper
 
 
-class MLGPLVM(InducingPointsModel):
+class MLGPLVM(MLGP):
     def __init__(self, y: np.ndarray, xdim: int, *,
                  x: np.ndarray = None,
                  kernel: Kernel = None,
                  num_inducing: int = 50,
                  likelihood: MixedLikelihoodWrapper,
                  ) -> None:
-        super().__init__(xdim, y.shape[1], y.shape[0], num_inducing)
         if x is None:
-            x = np.random.normal(size=(self.num_data, self.xdim))
-        elif x.shape[0] != self.num_data:
-            raise ValueError(f"First dimension of x and y must match, but x.shape={x.shape} and y.shape={y.shape}")
-        elif x.shape[1] != self.xdim:
+            x = np.random.normal(size=(y.shape[0], xdim))
+        elif x.shape[1] != xdim:
             raise ValueError(f"Second dimension of x must be xdim, but x.shape={x.shape} and xdim={self.xdim}")
-        if self.num_inducing > self.num_data:
-            raise ValueError(f"Can't have more inducing points than data, "
-                             f"but num_inducing={self.num_inducing} and y.shape={y.shape}")
-        inducing_indices = np.random.permutation(self.num_data)[:self.num_inducing]
-        z = x[inducing_indices]  # TODO: Maybe this should not be done if x is initialised randomly?
-        self.y = tf.convert_to_tensor(y, dtype=tf.float32)
-        if likelihood.num_dim != self.ydim:
-            raise ValueError(f"The likelihood must have as many dimensions as y, "
-                             f"but likelihood.num_dim={likelihood.num_dim} and y.shape={y.shape}")
-        self._likelihood = likelihood
-        self.kernel = kernel if (kernel is not None) else RBF()
+
+        super().__init__(x, y, kernel=kernel, num_inducing=num_inducing, likelihood=likelihood)
+        del self.x  # x is a latent variable in this model
 
         with tf.variable_scope("qx"):
             self.qx_mean = tf.get_variable("mean", shape=[self.xdim, self.num_data],
@@ -46,22 +32,6 @@ class MLGPLVM(InducingPointsModel):
             self.qx_scale = tf.identity(self.qx_log_scale
                                         - tf.matrix_diag(tf.matrix_diag_part(self.qx_log_scale))
                                         + tf.matrix_diag(tf.exp(tf.matrix_diag_part(self.qx_log_scale))), name="scale")
-        self.z = tf.get_variable("z", shape=[self.num_inducing, self.xdim], initializer=tf.constant_initializer(z))
-        with tf.variable_scope("qu"):
-            self.qu_mean = tf.get_variable("mean", shape=[self.ydim, self.num_inducing],
-                                           initializer=tf.random_normal_initializer(0.01))
-            self.qu_log_scale_vec = tf.get_variable("log_scale_vec",
-                                                    shape=[self.ydim, self.num_inducing * (self.num_inducing + 1) / 2],
-                                                    initializer=tf.zeros_initializer())
-            self.qu_log_scale = tfp.distributions.fill_triangular(self.qu_log_scale_vec, name="log_scale")
-            self.qu_scale = tf.identity(self.qu_log_scale
-                                        - tf.matrix_diag(tf.matrix_diag_part(self.qu_log_scale))
-                                        + tf.matrix_diag(tf.exp(tf.matrix_diag_part(self.qu_log_scale))), name="scale")
-        tf.losses.add_loss(self._loss())
-
-    def _loss(self) -> tf.Tensor:
-        loss = tf.negative(self._elbo(), name="elbo_loss")
-        return loss
 
     def _elbo(self) -> tf.Tensor:
         elbo = tf.identity(self._mc_expectation() - self._kl_qx_px() - self._kl_qu_pu(), name="elbo")
@@ -73,31 +43,6 @@ class MLGPLVM(InducingPointsModel):
             px = tfp.distributions.MultivariateNormalDiag(tf.zeros(self.num_data), tf.ones(self.num_data), name="px")
             kl = tf.reduce_sum(tfp.distributions.kl_divergence(qx, px, allow_nan_stats=False), axis=0, name="kl")
         return kl
-
-    def _kl_qu_pu(self) -> tf.Tensor:
-        with tf.name_scope("kl_qu_pu"):
-            qu = tfp.distributions.MultivariateNormalTriL(self.qu_mean, self.qu_scale, name="qu")
-            k_zz = self.kernel(self.z, name="k_zz")
-            # FIXME: The fix below does not seem to work on remote Linux computer, (because of TF<1.8>?)
-            # chol_zz = tf.cholesky(k_zz, name="chol_zz")
-            # pu = tfp.distributions.MultivariateNormalTriL(tf.zeros(self.num_inducing), chol_zz, name="pu")
-            chol_zz = tf.tile(tf.expand_dims(tf.cholesky(k_zz, name="chol_zz"), axis=0), multiples=[self.ydim, 1, 1])
-            pu = tfp.distributions.MultivariateNormalTriL(tf.zeros([self.ydim, self.num_inducing]), chol_zz, name="pu")
-            kl = tf.reduce_sum(tfp.distributions.kl_divergence(qu, pu, allow_nan_stats=False), axis=0, name="kl")
-        return kl
-
-    def _mc_expectation(self) -> tf.Tensor:
-        with tf.name_scope("mc_expectation"):
-            num_samples = int(1e1)
-            approx_exp_all = tfp.monte_carlo.expectation(f=self._log_prob, samples=self._sample_f(num_samples),
-                                                         name="approx_exp_all")
-            approx_exp = tf.reduce_sum(approx_exp_all, axis=[0, 1], name="approx_exp")
-        return approx_exp
-
-    def _log_prob(self, samples: tf.Tensor) -> tf.Tensor:
-        with tf.name_scope("log_prob"):
-            log_prob = self._likelihood.log_prob(tf.matrix_transpose(samples), self.y)
-        return log_prob
 
     def _sample_f(self, num_samples: int) -> tf.Tensor:
         with tf.name_scope("sample_f"):
@@ -140,36 +85,3 @@ class MLGPLVM(InducingPointsModel):
                                name="f_samples")
             assert f_samples.shape.as_list() == [num_samples, self.ydim, self.num_data]
         return f_samples
-
-    def predict(self, xs: np.ndarray) -> Tuple[tf.Tensor, tf.Tensor]:
-        # TODO: Not clear how to report the variances.
-        # Should we use qu_scale? Do we in the end want mean and std of f(x), h(f(x)) or p(y|x)=ExpFam(h(f(x)))?
-        # For now, we just report mean and std of ExpFam(h(f_mean(x)))
-        xs = tf.convert_to_tensor(xs, dtype=tf.float32)
-        k_zz = self.kernel(self.z)
-        k_zz_inv = tf.matrix_inverse(k_zz)
-        k_xs_z = self.kernel(xs, self.z)
-        f_mean = tf.matmul(tf.matmul(k_xs_z, k_zz_inv), self.qu_mean, transpose_b=True)
-        mean = tf.stack([likelihood(f_mean[:, i]).mean() for i, likelihood in enumerate(self._likelihood)], axis=1)
-        std = tf.stack([likelihood(f_mean[:, i]).stddev() for i, likelihood in enumerate(self._likelihood)], axis=1)
-
-        """
-        k_xsxs = self.kernel(xs)
-        f_cov = k_xsxs - tf.matmul(tf.matmul(k_xs_z, k_zz_inv), k_xs_z, transpose_b=True)
-        f_cov_pos = tf.maximum(f_cov, 0.)
-        f_std = tf.expand_dims(tf.sqrt(tf.matrix_diag_part(f_cov_pos)), axis=-1)
-        """
-        return mean, std
-
-    def create_summaries(self) -> None:
-        tf.summary.scalar("kl_qx_px", self._kl_qx_px(), family="Model")
-        tf.summary.scalar("kl_qu_pu", self._kl_qu_pu(), family="Model")
-        tf.summary.scalar("expectation", self._mc_expectation(), family="Model")
-        tf.summary.scalar("elbo_loss", self._loss(), family="Loss")
-        tf.summary.histogram("qx_mean", self.qx_mean)
-        tf.summary.histogram("qx_scale", tfp.distributions.fill_triangular_inverse(self.qx_scale))
-        tf.summary.histogram("z", self.z)
-        tf.summary.histogram("qu_mean", self.qu_mean)
-        tf.summary.histogram("qu_scale", tfp.distributions.fill_triangular_inverse(self.qu_scale))
-        self.kernel.create_summaries()
-        self._likelihood.create_summaries()
