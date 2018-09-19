f9962e1bb72a74b666af6ad06a64b693967ce1b3
Samuel Murray <samuelmu@kth.se>
2018-09-19 17:34:36 +0200

    Add MixedLikelihoodWrapper

--- tfgp/likelihood/__init__.py
+++ tfgp/likelihood/__init__.py
@@ -5,6 +5,7 @@ Module for all supported likelihoods
 from .likelihood import Likelihood
 from .bernoulli import Bernoulli
 from .categorical import Categorical
+from .mixed_likelihood_wrapper import MixedLikelihoodWrapper
 from .normal import Normal
 from .poisson import Poisson
 from .quantized_normal import QuantizedNormal
