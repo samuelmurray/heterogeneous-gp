a7966f76440e9bc1a2d72b0a69d3ea80a5546dab
Samuel Murray <samuelmu@kth.se>
2018-09-25 17:51:04 +0200

    Don't catch general exceptions

--- tfgp/tests/test_likelihoods.py
+++ tfgp/tests/test_likelihoods.py
@@ -30,16 +30,10 @@ class TestMixedLikelihoodWrapper(tf.test.TestCase):
         ber = Bernoulli()
         cat = Categorical(3)
         nor = Normal()
-        exception_thrown = False
-        try:
-            mixed = MixedLikelihoodWrapper([ber, cat, nor])
-            f = tf.constant(np.array([[[0.7, 0.4, 0.4, 0.2, 2.]]]), dtype=tf.float32)
-            y = tf.constant(np.array([[1, 1, 0, 0, 2.3]]), dtype=tf.float32)
-            mixed.log_prob(f, y)
-        except:
-            exception_thrown = True
-        finally:
-            self.assertFalse(exception_thrown)
+        mixed = MixedLikelihoodWrapper([ber, cat, nor])
+        f = tf.constant(np.array([[[0.7, 0.4, 0.4, 0.2, 2.]]]), dtype=tf.float32)
+        y = tf.constant(np.array([[1, 1, 0, 0, 2.3]]), dtype=tf.float32)
+        mixed.log_prob(f, y)
 
 
 class TestNormal(tf.test.TestCase):
