0efd53176e2e43d1a915f3c70efd7ed11475d85e
Samuel Murray <samuelmu@kth.se>
2019-02-19 12:55:40 +0100

    Add prints to asserts

--- tfgp/model/mlgp.py
+++ tfgp/model/mlgp.py
@@ -86,35 +86,44 @@ class MLGP(InducingPointsModel):
             # u = qu_mean + qu_scale * e_u, e_u ~ N(0,1)
             e_u = tf.random_normal(shape=[num_samples, self.ydim, self.num_inducing], name="e_u")
             u_sample = tf.add(self.qu_mean, tf.einsum("ijk,tik->tij", self.qu_scale, e_u), name="u_sample")
-            assert u_sample.shape.as_list() == [num_samples, self.ydim, self.num_inducing]
+            assert u_sample.shape.as_list() == [num_samples, self.ydim, self.num_inducing], "{} != {}".format(
+                u_sample.shape.as_list(), [num_samples, self.ydim, self.num_inducing])
 
             k_zx = self.kernel(self.z, self.x, name="k_zx")
-            assert k_zx.shape.as_list() == [self.num_inducing, self.num_data]
+            assert k_zx.shape.as_list() == [self.num_inducing, self.num_data], "{} != {}".format(
+                k_zx.shape.as_list(), [self.num_inducing, self.num_data])
             k_xx = self.kernel(self.x, name="k_xx")
-            assert k_xx.shape.as_list() == [self.num_data, self.num_data]
+            assert k_xx.shape.as_list() == [self.num_data, self.num_data], "{} != {}".format(
+                k_xx.shape.as_list(), [self.num_data, self.num_data])
 
             # a = Kzz^(-1) * Kzx
             a = tf.matmul(k_zz_inv, k_zx, name="a")
-            assert a.shape.as_list() == [self.num_inducing, self.num_data]
+            assert a.shape.as_list() == [self.num_inducing, self.num_data], "{} != {}".format(
+                a.shape.as_list(), [self.num_inducing, self.num_data])
 
             # b = Kxx - Kxz * Kzz^(-1) * Kzx
             full_b = tf.subtract(k_xx, tf.matmul(k_zx, a, transpose_a=True), name="full_b")
-            assert full_b.shape.as_list() == [self.num_data, self.num_data]
+            assert full_b.shape.as_list() == [self.num_data, self.num_data], "{} != {}".format(
+                full_b.shape.as_list(), [self.num_data, self.num_data])
             b = tf.matrix_diag_part(full_b, name="diag_b")
-            assert b.shape.as_list() == [self.num_data]
+            assert b.shape.as_list() == [self.num_data], "{} != {}".format(
+                b.shape.as_list(), [self.num_data])
             b = tf.maximum(b, 1e-16, name="pos_b")  # Sometimes b is small negative, which will crash in sqrt(b)
 
             a = tf.tile(tf.expand_dims(a, axis=0), multiples=[num_samples, 1, 1])
-            assert a.shape.as_list() == [num_samples, self.num_inducing, self.num_data]
+            assert a.shape.as_list() == [num_samples, self.num_inducing, self.num_data], "{} != {}".format(
+                a.shape.as_list(), [num_samples, self.num_inducing, self.num_data])
             b = tf.tile(tf.expand_dims(b, axis=0), multiples=[num_samples, 1])
-            assert b.shape.as_list() == [num_samples, self.num_data]
+            assert b.shape.as_list() == [num_samples, self.num_data], "{} != {}".format(
+                b.shape.as_list(), [num_samples, self.num_inducing])
 
             # f = a.T * u + sqrt(b) * e_f, e_f ~ N(0,1)
             e_f = tf.random_normal(shape=[num_samples, self.ydim, self.num_data], name="e_f")
             ua = tf.matmul(u_sample, a)
             bef = tf.multiply(tf.expand_dims(tf.sqrt(b), axis=1), e_f)
             f_samples = tf.add(ua, bef, name="f_samples")
-            assert f_samples.shape.as_list() == [num_samples, self.ydim, self.num_data], f"{f_samples.shape.as_list()}"
+            assert f_samples.shape.as_list() == [num_samples, self.ydim, self.num_data], "{} != {}".format(
+                f_samples.shape.as_list(), [num_samples, self.ydim, self.num_data])
         return f_samples
 
     def predict(self, xs: np.ndarray) -> Tuple[tf.Tensor, tf.Tensor]:
--- tfgp/model/mlgplvm.py
+++ tfgp/model/mlgplvm.py
@@ -49,29 +49,36 @@ class MLGPLVM(MLGP):
             # x = qx_mean + qx_std * e_x, e_x ~ N(0,1)
             e_x = tf.random_normal(shape=[num_samples, self.num_data, self.xdim], name="e_x")
             x_sample = tf.add(self.qx_mean, tf.sqrt(self.qx_var) * e_x, name="x_sample")
-            assert x_sample.shape.as_list() == [num_samples, self.num_data, self.xdim]
+            assert x_sample.shape.as_list() == [num_samples, self.num_data, self.xdim], "{} != {}".format(
+                x_sample.shape.as_list(), [num_samples, self.num_data, self.xdim])
 
             # u = qu_mean + qu_scale * e_u, e_u ~ N(0,1)
             e_u = tf.random_normal(shape=[num_samples, self.ydim, self.num_inducing], name="e_u")
             u_sample = tf.add(self.qu_mean, tf.einsum("ijk,tik->tij", self.qu_scale, e_u), name="u_sample")
-            assert u_sample.shape.as_list() == [num_samples, self.ydim, self.num_inducing]
+            assert u_sample.shape.as_list() == [num_samples, self.ydim, self.num_inducing], "{} != {}".format(
+                u_sample.shape.as_list(), [num_samples, self.ydim, self.num_inducing])
 
             k_zx = self.kernel(tf.tile(tf.expand_dims(self.z, axis=0), multiples=[num_samples, 1, 1]),
                                x_sample,
                                name="k_zx")
-            assert k_zx.shape.as_list() == [num_samples, self.num_inducing, self.num_data]
+            assert k_zx.shape.as_list() == [num_samples, self.num_inducing, self.num_data], "{} != {}".format(
+                k_zx.shape.as_list(), [num_samples, self.num_inducing, self.num_data])
             k_xx = self.kernel(x_sample, name="k_xx")
-            assert k_xx.shape.as_list() == [num_samples, self.num_data, self.num_data]
+            assert k_xx.shape.as_list() == [num_samples, self.num_data, self.num_data], "{} != {}".format(
+                k_xx.shape.as_list(), [num_samples, self.num_data, self.num_data])
 
             # a = Kzz^(-1) * Kzx
             a = tf.einsum("ij,sjk->sik", k_zz_inv, k_zx, name="a")
-            assert a.shape.as_list() == [num_samples, self.num_inducing, self.num_data]
+            assert a.shape.as_list() == [num_samples, self.num_inducing, self.num_data], "{} != {}".format(
+                a.shape.as_list(), [num_samples, self.num_inducing, self.num_data])
 
             # b = Kxx - Kxz * Kzz^(-1) * Kzx
             full_b = tf.subtract(k_xx, tf.matmul(k_zx, a, transpose_a=True), name="full_b")
-            assert full_b.shape.as_list() == [num_samples, self.num_data, self.num_data]
+            assert full_b.shape.as_list() == [num_samples, self.num_data, self.num_data], "{} != {}".format(
+                full_b.shape.as_list(), [num_samples, self.num_data, self.num_data])
             b = tf.matrix_diag_part(full_b, name="diag_b")
-            assert b.shape.as_list() == [num_samples, self.num_data]
+            assert b.shape.as_list() == [num_samples, self.num_data], "{} != {}".format(
+                b.shape.as_list(), [num_samples, self.num_data])
             b = tf.maximum(b, 1e-16, name="pos_b")  # Sometimes b is small negative, which will crash in sqrt(b)
 
             # f = a.T * u + sqrt(b) * e_f, e_f ~ N(0,1)
@@ -79,7 +86,8 @@ class MLGPLVM(MLGP):
             f_samples = tf.add(tf.matmul(u_sample, a),
                                tf.multiply(tf.expand_dims(tf.sqrt(b), axis=1), e_f),
                                name="f_samples")
-            assert f_samples.shape.as_list() == [num_samples, self.ydim, self.num_data]
+            assert f_samples.shape.as_list() == [num_samples, self.ydim, self.num_data], "{} != {}".format(
+                f_samples.shape.as_list(), [num_samples, self.ydim, self.num_data])
         return f_samples
 
     def impute(self) -> tf.Tensor:
