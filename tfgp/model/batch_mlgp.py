718e66f5553229b29d1828c6dfb41cd15b2dc0a0
Samuel Murray <samuelmu@kth.se>
2019-02-19 18:58:45 +0100

    Change _sample_f to work with any x, for ease of code reusage

--- tfgp/model/batch_mlgp.py
+++ tfgp/model/batch_mlgp.py
@@ -32,49 +32,5 @@ class BatchMLGP(MLGP):
             log_prob = self._likelihood.log_prob(tf.matrix_transpose(samples), self.y_batch)
         return log_prob
 
-    def _sample_f(self) -> tf.Tensor:
-        with tf.name_scope("sample_f"):
-            k_zz = self.kernel(self.z, name="k_zz")
-            k_zz_inv = tf.matrix_inverse(k_zz, name="k_zz_inv")
-
-            k_zx = self.kernel(self.z, self.x_batch, name="k_zx")
-            assert k_zx.shape.as_list() == [self.num_inducing, self._batch_size], "{} != {}".format(
-                k_zx.shape.as_list(), [self.num_inducing, self._batch_size])
-            k_xx = self.kernel(self.x_batch, name="k_xx")
-            assert k_xx.shape.as_list() == [self._batch_size, self._batch_size], "{} != {}".format(
-                k_xx.shape.as_list(), [self._batch_size, self._batch_size])
-
-            # a = Kzz^(-1) * Kzx
-            a = tf.matmul(k_zz_inv, k_zx, name="a")
-            assert a.shape.as_list() == [self.num_inducing, self._batch_size], "{} != {}".format(
-                a.shape.as_list(), [self.num_inducing, self._batch_size])
-
-            # K~ = Kxx - Kxz * Kzz^(-1) * Kzx
-            k_tilde_full = tf.subtract(k_xx, tf.matmul(k_zx, a, transpose_a=True), name="k_tilde_full")
-            assert k_tilde_full.shape.as_list() == [self._batch_size, self._batch_size], "{} != {}".format(
-                k_tilde_full.shape.as_list(), [self._batch_size, self._batch_size])
-
-            k_tilde = tf.matrix_diag_part(k_tilde_full, name="diag_b")
-            assert k_tilde.shape.as_list() == [self._batch_size], "{} != {}".format(
-                k_tilde.shape.as_list(), [self._batch_size])
-
-            k_tilde_pos = tf.maximum(k_tilde, 1e-16, name="pos_b")  # k_tilde can't be negative
-
-            a_tiled = tf.tile(tf.expand_dims(a, axis=0), multiples=[self._num_samples, 1, 1])
-            assert a_tiled.shape.as_list() == [self._num_samples, self.num_inducing, self._batch_size], "{} != {}".format(
-                a_tiled.shape.as_list(), [self._num_samples, self.num_inducing, self._batch_size])
-
-            k_tilde_pos_tiled = tf.tile(tf.expand_dims(k_tilde_pos, axis=0), multiples=[self._num_samples, 1])
-            assert k_tilde_pos_tiled.shape.as_list() == [self._num_samples, self._batch_size], "{} != {}".format(
-                k_tilde_pos_tiled.shape.as_list(), [self._num_samples, self._batch_size])
-
-            # f = a.T * u + sqrt(K~) * e_f, e_f ~ N(0,1)
-            u_samples = self._sample_us()
-            e_f = tf.random_normal(shape=[self._num_samples, self.ydim, self._batch_size], name="e_f")
-            f_mean = tf.matmul(u_samples, a_tiled, name="f_mean")
-            f_noise = tf.multiply(tf.expand_dims(tf.sqrt(k_tilde_pos_tiled), axis=1), e_f, name="f_noise")
-            f_samples = tf.add(f_mean, f_noise, name="f_samples")
-            assert f_samples.shape.as_list() == [self._num_samples, self.ydim, self._batch_size], "{} != {}".format(
-                f_samples.shape.as_list(), [self._num_samples, self.ydim, self._batch_size])
-
-        return f_samples
+    def _sample_or_return_x(self):
+        return self.x_batch
