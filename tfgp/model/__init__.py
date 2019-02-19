39ac011d9374c2a3e3e63d692b2853dc91cd9425
Samuel Murray <samuelmu@kth.se>
2019-02-19 16:28:52 +0100

    Add BatchMLGP

--- tfgp/model/__init__.py
+++ tfgp/model/__init__.py
@@ -3,12 +3,14 @@ Package for all supported models
 """
 
 __all__ = [
+    "BatchMLGP",
     "GP",
     "GPLVM",
     "MLGP",
     "MLGPLVM",
 ]
 
+from .batch_mlgp import BatchMLGP
 from .gp import GP
 from .gplvm import GPLVM
 from .mlgp import MLGP
