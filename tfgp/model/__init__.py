0d6e1cc74d45722971549b434867e0f4e06f2f23
Samuel Murray <samuelmu@kth.se>
2019-02-19 19:03:27 +0100

    Expose abstract classes

--- tfgp/model/__init__.py
+++ tfgp/model/__init__.py
@@ -6,12 +6,16 @@ __all__ = [
     "BatchMLGP",
     "GP",
     "GPLVM",
+    "InducingPointsModel",
     "MLGP",
     "MLGPLVM",
+    "Model"
 ]
 
 from .batch_mlgp import BatchMLGP
 from .gp import GP
 from .gplvm import GPLVM
+from .inducing_points_model import InducingPointsModel
 from .mlgp import MLGP
 from .mlgplvm import MLGPLVM
+from .model import Model
