dd529ff2754a954ed6e59f1df7a65f06a2bb4f80
Samuel Murray <samuelmu@kth.se>
2019-02-25 17:36:15 +0100

    Don't pass batch_size to model, update testing of impute to account for changes in method

--- tfgp/tests/test_vae_mlgplvm.py
+++ tfgp/tests/test_vae_mlgplvm.py
@@ -19,8 +19,7 @@ class TestVAEMLGPLVM(tf.test.TestCase):
             likelihood = MixedLikelihoodWrapper([Normal() for _ in range(self.output_dim)])
             self.batch_size = 5
             num_hidden_units = 10
-            self.m = VAEMLGPLVM(y, self.latent_dim, likelihood=likelihood, batch_size=self.batch_size,
-                                num_hidden_units=num_hidden_units)
+            self.m = VAEMLGPLVM(y, self.latent_dim, likelihood=likelihood, num_hidden_units=num_hidden_units)
             self.m.initialize()
 
     def tearDown(self) -> None:
@@ -49,19 +48,18 @@ class TestVAEMLGPLVM(tf.test.TestCase):
             learning_rate = 0.1
             optimizer = tf.train.RMSPropOptimizer(learning_rate)
             train_all = optimizer.minimize(loss, var_list=tf.trainable_variables())
+
             init = tf.global_variables_initializer()
             indices = np.arange(self.batch_size)
             feed_dict = {self.m.batch_indices: indices}
+            encoder_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="vae_mlgplvm/encoder")
             with tf.Session() as sess:
                 sess.run(init)
-                encoder_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="vae_mlgplvm/encoder")
                 variables_before = sess.run(encoder_variables)
-
-                # Run one training op
                 sess.run(train_all, feed_dict=feed_dict)
-
                 variables_after = sess.run(encoder_variables)
 
+            # Variables in encoder should change
             for i, (before, after) in enumerate(zip(variables_before, variables_after)):
                 with self.subTest(status_code=[encoder_variables[i]]):
                     self.assertTrue((before != after).all())
@@ -69,10 +67,14 @@ class TestVAEMLGPLVM(tf.test.TestCase):
     def test_impute(self) -> None:
         with tf.variable_scope("vae_mlgplvm", reuse=tf.AUTO_REUSE):
             init = tf.global_variables_initializer()
+            indices = np.arange(self.batch_size)
+            feed_dict = {self.m.batch_indices: indices}
             with tf.Session() as sess:
                 sess.run(init)
                 y_impute = self.m.impute()
-            self.assertShapeEqual(np.empty((self.num_data, self.output_dim)), y_impute)
+                y_impute_arr = sess.run(y_impute, feed_dict=feed_dict)
+            self.assertEqual([None, self.output_dim], y_impute.shape.as_list())
+            self.assertEqual([self.batch_size, self.output_dim], list(y_impute_arr.shape))
 
 
 if __name__ == "__main__":
