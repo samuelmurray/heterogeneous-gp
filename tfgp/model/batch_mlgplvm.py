52a22ef10d5538140b652fe8e00277d4b40e7c91
Samuel Murray <samuelmu@kth.se>
2019-02-25 14:24:48 +0100

    Add names to tensors

--- tfgp/model/batch_mlgplvm.py
+++ tfgp/model/batch_mlgplvm.py
@@ -17,14 +17,16 @@ class BatchMLGPLVM(MLGPLVM):
                  likelihood: MixedLikelihoodWrapper,
                  ) -> None:
         super().__init__(y=y, xdim=xdim, x=x, kernel=kernel, num_inducing=num_inducing, likelihood=likelihood)
+
         self._batch_size = batch_size
         if self.batch_size > self.num_data:
             raise ValueError(f"Can't have larger batch size the number of data,"
                              f"but batch_size={batch_size} and y.shape={y.shape}")
-        self.batch_indices = tf.placeholder(shape=self.batch_size, dtype=tf.int32)
-        self.qx_mean_batch = tf.gather(self.qx_mean, self.batch_indices)
-        self.qx_var_batch = tf.gather(self.qx_var, self.batch_indices)
-        self.y_batch = tf.gather(self.y, self.batch_indices)
+
+        self.batch_indices = tf.placeholder(shape=self.batch_size, dtype=tf.int32, name="batch_indices")
+        self.qx_mean_batch = tf.gather(self.qx_mean, self.batch_indices, name="qx_mean_batch")
+        self.qx_var_batch = tf.gather(self.qx_var, self.batch_indices, name="qx_var_batch")
+        self.y_batch = tf.gather(self.y, self.batch_indices, name="y_batch")
 
     @property
     def batch_size(self) -> int:
