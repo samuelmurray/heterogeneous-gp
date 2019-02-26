498056a28d9440791ba2c1b050d69ee01275ce9b
Samuel Murray <samuelmu@kth.se>
2019-02-26 12:14:43 +0100

    Fix name typo

--- tfgp/tests/test_inducing_points_model.py
+++ tfgp/tests/test_inducing_points_model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tfgp.model.inducing_points_model import InducingPointsModel
 
 
-class TestModel(tf.test.TestCase):
+class TestInducingPointsModel(tf.test.TestCase):
     def tearDown(self) -> None:
         tf.reset_default_graph()
 
