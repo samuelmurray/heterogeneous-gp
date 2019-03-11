d928febe743db4ccf6c9d59ae97678183f7f2a5d
Samuel Murray <samuelmu@kth.se>
2019-03-11 15:20:48 +0100

    Add util functions for imputation experiments

--- tfgp/util/__init__.py
+++ tfgp/util/__init__.py
@@ -6,10 +6,16 @@ __all__ = [
     "knn_abs_error",
     "knn_error",
     "knn_rmse",
+    "nrmse_mean",
+    "nrmse_range",
     "pca_reduce",
+    "remove_data",
 ]
 
 from .util import knn_abs_error
 from .util import knn_error
 from .util import knn_rmse
+from .util import nrmse_mean
+from .util import nrmse_range
 from .util import pca_reduce
+from .util import remove_data
--- tfgp/util/util.py
+++ tfgp/util/util.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
+from tfgp.likelihood import MixedLikelihoodWrapper
+
 
 def knn_abs_error(x: np.ndarray, labels: np.ndarray, k: int) -> float:
     knn = NearestNeighbors(k).fit(x)
@@ -23,6 +25,29 @@ def knn_rmse(x: np.ndarray, labels: np.ndarray, k: int) -> float:
     return np.sqrt(np.mean(np.square(labels - guess)))
 
 
+def _nrmse(y_imputation: np.ndarray, y_missing: np.ndarray, y_true: np.ndarray, *, use_mean: bool) -> float:
+    nan_mask = np.isnan(y_missing)
+    y_filtered = y_true.copy()
+    y_filtered[~nan_mask] = np.nan
+    error = y_imputation - y_filtered
+    square_error = error ** 2
+    mean_square_error = np.nanmean(square_error, axis=0)
+    rmse = np.sqrt(mean_square_error)
+    if use_mean:
+        nrmse = rmse / np.nanmean(y_filtered, axis=0)
+    else:
+        nrmse = rmse / (np.max(y_filtered) - np.min(y_filtered))
+    return nrmse
+
+
+def nrmse_mean(y_imputation: np.ndarray, y_missing: np.ndarray, y_true: np.ndarray) -> float:
+    return _nrmse(y_imputation, y_missing, y_true, use_mean=True)
+
+
+def nrmse_range(y_imputation: np.ndarray, y_missing: np.ndarray, y_true: np.ndarray) -> float:
+    return _nrmse(y_imputation, y_missing, y_true, use_mean=False)
+
+
 def pca_reduce(x: np.ndarray, latent_dim: int, *, whiten: bool = False) -> np.ndarray:
     """
     Reduce the dimensionality of x to latent_dim with PCA.
@@ -38,3 +63,17 @@ def pca_reduce(x: np.ndarray, latent_dim: int, *, whiten: bool = False) -> np.nd
     if whiten:
         x_reduced /= x_reduced.std(axis=0)
     return x_reduced
+
+
+def remove_data(y: np.ndarray, frac: float, likelihood: MixedLikelihoodWrapper) -> np.ndarray:
+    y_noisy = y.copy()
+    num_missing = int(frac * likelihood.num_likelihoods)
+    dims_missing = np.repeat([np.arange(likelihood.num_likelihoods)], y.shape[0], axis=0)
+    _ = np.apply_along_axis(np.random.shuffle, 1, dims_missing)
+    dims_missing = dims_missing[:, :num_missing]
+    idx = np.zeros(y.shape, dtype=bool)
+    for i in range(dims_missing.shape[0]):
+        for j in range(dims_missing.shape[1]):
+            idx[i, likelihood._slices[dims_missing[i, j]]] = True
+    y_noisy[idx] = np.nan
+    return y_noisy
