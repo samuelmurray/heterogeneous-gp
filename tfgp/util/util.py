28338c2bcac2b37317d625e228624acae39ce2e9
Samuel Murray <samuelmu@kth.se>
2019-03-11 16:18:08 +0100

    Remove old doc for pca_reduce

--- tfgp/util/util.py
+++ tfgp/util/util.py
@@ -49,13 +49,6 @@ def nrmse_range(y_imputation: np.ndarray, y_missing: np.ndarray, y_true: np.ndar
 
 
 def pca_reduce(x: np.ndarray, latent_dim: int, *, whiten: bool = False) -> np.ndarray:
-    """
-    Reduce the dimensionality of x to latent_dim with PCA.
-    :param x: data array of size N (number of points) x D (dimensions)
-    :param latent_dim: Number of latent dimensions (< D)
-    :param whiten: if True, also scales the data so that each dimension has unit variance
-    :return: PCA projection array of size N x latent_dim.
-    """
     assert latent_dim <= x.shape[1], "Cannot have more latent dimensions than observed"
     _, eigen_vecs = np.linalg.eigh(np.cov(x.T))
     w = eigen_vecs[:, -latent_dim:]
