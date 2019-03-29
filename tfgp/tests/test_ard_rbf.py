853220797be3d5a404dceaefafb2426622f5adfe
Samuel Murray <samuelmu@kth.se>
2019-03-29 16:04:43 +0100

    Rename attribute to kernel

--- tfgp/tests/test_ard_rbf.py
+++ tfgp/tests/test_ard_rbf.py
@@ -10,7 +10,7 @@ class TestARDRBF(tf.test.TestCase):
         np.random.seed(1363431413)
         tf.random.set_random_seed(1534135313)
         with tf.variable_scope("ardrbf", reuse=tf.AUTO_REUSE):
-            self.kern = ARDRBF(1., 0.5, xdim=5)
+            self.kernel = ARDRBF(1., 0.5, xdim=5)
 
     def tearDown(self) -> None:
         tf.reset_default_graph()
@@ -19,13 +19,13 @@ class TestARDRBF(tf.test.TestCase):
         a = tf.convert_to_tensor(np.random.normal(size=(10, 5)), dtype=tf.float32)
         b = tf.convert_to_tensor(np.random.normal(size=(12, 4)), dtype=tf.float32)
         with self.assertRaises(ValueError):
-            self.kern(a, b)
+            self.kernel(a, b)
 
     def test_equal_to_sklearn(self) -> None:
         a = np.random.normal(size=(7, 5))
         b = np.random.normal(size=(6, 5))
         k_sklearn = rbf_kernel(a, b, gamma=0.5)
-        k_ard_rbf = self.kern(tf.convert_to_tensor(a, dtype=tf.float32), tf.convert_to_tensor(b, dtype=tf.float32))
+        k_ard_rbf = self.kernel(tf.convert_to_tensor(a, dtype=tf.float32), tf.convert_to_tensor(b, dtype=tf.float32))
         init = tf.global_variables_initializer()
         with self.test_session() as sess:
             sess.run(init)
@@ -34,23 +34,23 @@ class TestARDRBF(tf.test.TestCase):
 
     def test_simple(self) -> None:
         a = tf.convert_to_tensor(np.random.normal(size=(10, 5)), dtype=tf.float32)
-        k = self.kern(a)
+        k = self.kernel(a)
         self.assertShapeEqual(np.empty([10, 10]), k)
 
     def test_extended(self) -> None:
         a = tf.convert_to_tensor(np.random.normal(size=(2, 10, 5)), dtype=tf.float32)
-        k = self.kern(a)
+        k = self.kernel(a)
         self.assertShapeEqual(np.empty([2, 10, 10]), k)
 
     def test_full(self) -> None:
         a = tf.convert_to_tensor(np.random.normal(size=(2, 10, 5)), dtype=tf.float32)
         b = tf.convert_to_tensor(np.random.normal(size=(2, 12, 5)), dtype=tf.float32)
-        k = self.kern(a, b)
+        k = self.kernel(a, b)
         self.assertShapeEqual(np.empty([2, 10, 12]), k)
-        self.kern.create_summaries()
+        self.kernel.create_summaries()
 
     def test_create_summary(self) -> None:
-        self.kern.create_summaries()
+        self.kernel.create_summaries()
         merged_summary = tf.summary.merge_all()
         self.assertIsNotNone(merged_summary)
 
--- tfgp/tests/test_rbf.py
+++ tfgp/tests/test_rbf.py
@@ -10,7 +10,7 @@ class TestRBF(tf.test.TestCase):
         np.random.seed(1363431413)
         tf.random.set_random_seed(1534135313)
         with tf.variable_scope("rbf", reuse=tf.AUTO_REUSE):
-            self.rbf = RBF(1., 0.5)
+            self.kernel = RBF(1., 0.5)
 
     def tearDown(self) -> None:
         tf.reset_default_graph()
@@ -19,29 +19,29 @@ class TestRBF(tf.test.TestCase):
         a = tf.convert_to_tensor(np.random.normal(size=(10, 5)), dtype=tf.float32)
         b = tf.convert_to_tensor(np.random.normal(size=(12, 4)), dtype=tf.float32)
         with self.assertRaises(ValueError):
-            self.rbf(a, b)
+            self.kernel(a, b)
 
     def test_simple(self) -> None:
         a = tf.convert_to_tensor(np.random.normal(size=(10, 5)), dtype=tf.float32)
-        k = self.rbf(a)
+        k = self.kernel(a)
         self.assertShapeEqual(np.empty([10, 10]), k)
 
     def test_extended(self) -> None:
         a = tf.convert_to_tensor(np.random.normal(size=(2, 10, 5)), dtype=tf.float32)
-        k = self.rbf(a)
+        k = self.kernel(a)
         self.assertShapeEqual(np.empty([2, 10, 10]), k)
 
     def test_full(self) -> None:
         a = tf.convert_to_tensor(np.random.normal(size=(2, 10, 5)), dtype=tf.float32)
         b = tf.convert_to_tensor(np.random.normal(size=(2, 12, 5)), dtype=tf.float32)
-        k = self.rbf(a, b)
+        k = self.kernel(a, b)
         self.assertShapeEqual(np.empty([2, 10, 12]), k)
 
     def test_equal_to_sklearn(self) -> None:
         a = np.random.normal(size=(5, 5))
         b = np.random.normal(size=(6, 5))
         k_sklearn = rbf_kernel(a, b, gamma=0.5)
-        k_rbf = self.rbf(tf.convert_to_tensor(a, dtype=tf.float32), tf.convert_to_tensor(b, dtype=tf.float32))
+        k_rbf = self.kernel(tf.convert_to_tensor(a, dtype=tf.float32), tf.convert_to_tensor(b, dtype=tf.float32))
         init = tf.global_variables_initializer()
         with self.test_session() as sess:
             sess.run(init)
@@ -49,7 +49,7 @@ class TestRBF(tf.test.TestCase):
         self.assertAllClose(k_ab, k_sklearn)
 
     def test_create_summary(self) -> None:
-        self.rbf.create_summaries()
+        self.kernel.create_summaries()
         merged_summary = tf.summary.merge_all()
         self.assertIsNotNone(merged_summary)
 
