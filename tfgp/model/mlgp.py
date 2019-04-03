4d6ff586e01a0d3642b5e5edbf04ecc2e9ee49fb
Samuel Murray <samuelmu@kth.se>
2019-04-03 14:58:02 +0200

    Extract method _compute_k_tilde

--- tfgp/model/mlgp.py
+++ tfgp/model/mlgp.py
@@ -102,20 +102,13 @@ class MLGP(InducingPointsModel):
             k_xx = self.kernel(x, name="k_xx")
 
             a = self._compute_a(k_zz_inv, k_zx)
-
-            # K~ = Kxx - Kxz * Kzz^(-1) * Kzx
-            k_tilde_full = tf.subtract(k_xx, tf.matmul(k_zx, a, transpose_a=True),
-                                       name="k_tilde_full")
-            k_tilde = tf.matrix_diag_part(k_tilde_full, name="diag_b")
-            k_tilde_pos = tf.maximum(k_tilde, 1e-16, name="pos_b")  # k_tilde can't be negative
-            k_tilde_pos_tiled = tf.tile(tf.expand_dims(k_tilde_pos, axis=0),
-                                        multiples=[self.num_samples, 1])
+            k_tilde = self._compute_k_tilde(k_xx, k_zx, a)
 
             # f = a.T * u + sqrt(K~) * e_f, e_f ~ N(0,1)
             u_sample = self._sample_u()
             e_f = tf.random_normal(shape=[self.num_samples, self.y_dim, num_data], name="e_f")
-            f_mean = tf.matmul(u_sample, a_tiled, name="f_mean")
-            f_noise = tf.multiply(tf.expand_dims(tf.sqrt(k_tilde_pos_tiled), axis=1), e_f,
+            f_mean = tf.matmul(u_sample, a, name="f_mean")
+            f_noise = tf.multiply(tf.expand_dims(tf.sqrt(k_tilde), axis=1), e_f,
                                   name="f_noise")
             f_samples = tf.add(f_mean, f_noise, name="f_samples")
         return f_samples
@@ -131,6 +124,16 @@ class MLGP(InducingPointsModel):
         a = tf.matmul(k_zz_inv, k_zx, name="a")
         a_tiled = tf.tile(tf.expand_dims(a, axis=0), multiples=[self.num_samples, 1, 1])
         return a_tiled
+
+    def _compute_k_tilde(self, k_xx, k_zx, a):
+        # K~ = Kxx - Kxz * Kzz^(-1) * Kzx
+        k_tilde_full = tf.subtract(k_xx, tf.matmul(k_zx, a, transpose_a=True),
+                                   name="k_tilde_full")
+        k_tilde = tf.matrix_diag_part(k_tilde_full, name="diag_b")
+        k_tilde_pos = tf.maximum(k_tilde, 1e-16, name="pos_b")  # k_tilde can't be negative
+        k_tilde_pos_tiled = tf.tile(tf.expand_dims(k_tilde_pos, axis=0),
+                                    multiples=[self.num_samples, 1])
+        return k_tilde_pos_tiled
 
     def _sample_u(self) -> tf.Tensor:
         # u = qu_mean + qu_scale * e_u, e_u ~ N(0,1)
