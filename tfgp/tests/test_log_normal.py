1191789d57d5376b7e36635ec4aac9a4f3a62da8
Samuel Murray <samuelmu@kth.se>
2019-04-03 16:53:42 +0200

    Split test_call in two tests

--- tfgp/tests/test_log_normal.py
+++ tfgp/tests/test_log_normal.py
@@ -11,12 +11,17 @@ class TestLogNormal(tf.test.TestCase):
     def tearDown(self) -> None:
         tf.reset_default_graph()
 
-    def test_call(self) -> None:
+    def test_call_return_type(self) -> None:
         shape = (10, 5)
         f = tf.ones(shape)
         ret = self.likelihood(f)
         self.assertIsInstance(ret, tfp.distributions.LogNormal)
-        self.assertEqual((10, 5), ret.batch_shape)
+
+    def test_call_return_shape(self) -> None:
+        shape = (10, 5)
+        f = tf.ones(shape)
+        ret = self.likelihood(f)
+        self.assertEqual(shape, ret.batch_shape)
 
     def test_create_summary(self) -> None:
         self.likelihood.create_summaries()
--- tfgp/tests/test_one_hot_categorical.py
+++ tfgp/tests/test_one_hot_categorical.py
@@ -11,13 +11,17 @@ class TestOneHotCategorical(tf.test.TestCase):
     def tearDown(self) -> None:
         tf.reset_default_graph()
 
-    def test_call(self) -> None:
+    def test_call_return_type(self) -> None:
         shape = (10, 5)
         f = tf.ones(shape)
         ret = self.likelihood(f)
         self.assertIsInstance(ret, tfp.distributions.OneHotCategorical)
-        self.assertEqual(shape[0], ret.batch_shape[0])
-        self.assertEqual(shape[1], ret.event_shape[0])
+
+    def test_call_return_shape(self) -> None:
+        shape = (10, 5)
+        f = tf.ones(shape)
+        ret = self.likelihood(f)
+        self.assertEqual(shape, (ret.batch_shape, ret.event_shape))
 
     def test_create_summary(self) -> None:
         self.likelihood.create_summaries()
