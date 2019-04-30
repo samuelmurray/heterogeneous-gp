49d61c246d6ab5a6c69cb85f73381db4a3e18235
Samuel Murray <samuelmu@kth.se>
2019-04-30 17:52:57 +0200

    Change to make first logit zero

--- tfgp/likelihood/one_hot_categorical.py
+++ tfgp/likelihood/one_hot_categorical.py
@@ -9,7 +9,10 @@ class OneHotCategorical(Likelihood):
         super().__init__(num_classes - 1, num_classes)
 
     def __call__(self, f: tf.Tensor) -> tfp.distributions.OneHotCategorical:
-        return tfp.distributions.OneHotCategorical(logits=f)
+        zeros = tf.zeros([tf.rank(f) - 1, 2], dtype=tf.int32)
+        padding = tf.concat([zeros, [[1, 0]]], axis=0)
+        f_extended = tf.pad(f, padding)
+        return tfp.distributions.OneHotCategorical(logits=f_extended)
 
     def create_summaries(self) -> None:
         pass
