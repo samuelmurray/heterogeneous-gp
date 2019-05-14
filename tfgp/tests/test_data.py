b86ae29015591fe23dc830c755ba6a076e4bc2cf
Samuel Murray <samuelmu@kth.se>
2019-05-14 15:21:02 +0200

    Stub out tests that depend on data files

--- tfgp/tests/test_data.py
+++ tfgp/tests/test_data.py
@@ -224,5 +224,83 @@ class TestUnsupervisedData(tf.test.TestCase):
             self.assertIsInstance(labels, np.ndarray)
 
 
+class TestUnsupervisedDataFileNotFound(tf.test.TestCase):
+    def setUp(self) -> None:
+        self.num_data = 10
+        self.stubs = tf.test.StubOutForTesting()
+        self.stubs.Set(os.path, "isfile", lambda x: False)
+
+    def tearDown(self) -> None:
+        self.stubs.CleanUp()
+        tf.reset_default_graph()
+
+    def test_abalone(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_abalone(self.num_data)
+
+    def test_adult(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_adult(self.num_data)
+
+    def test_atr(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_atr(self.num_data)
+
+    def test_binaryalphadigits(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_binaryalphadigits(self.num_data)
+
+    def test_binaryalphadigits_test(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_binaryalphadigits_test(self.num_data)
+
+    def test_cleveland(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_cleveland(self.num_data)
+
+    def test_cleveland_quantized(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_cleveland_quantized(self.num_data)
+
+    def test_default_credit(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_default_credit(self.num_data)
+
+    def test_mimic(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_mimic(self.num_data)
+
+    def test_mimic_labeled(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_mimic_labeled(self.num_data)
+
+    def test_mimic_test(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_mimic_test(self.num_data)
+
+    def test_wine(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_wine(self.num_data)
+
+    def test_wine_pos(self) -> None:
+        with self.assertRaises(FileNotFoundError):
+            data.make_wine_pos(self.num_data)
+
+
+class TestUnsupervisedDataModuleNotFound(tf.test.TestCase):
+    def setUp(self) -> None:
+        self.num_data = 10
+        self.stubs = tf.test.StubOutForTesting()
+        self.stubs.Set(importlib.util, "find_spec", lambda x: False)
+
+    def tearDown(self) -> None:
+        self.stubs.CleanUp()
+        tf.reset_default_graph()
+
+    def test_oilflow(self) -> None:
+        with self.assertRaises(ModuleNotFoundError):
+            data.make_oilflow(self.num_data)
+
+
 if __name__ == "__main__":
     tf.test.main()
