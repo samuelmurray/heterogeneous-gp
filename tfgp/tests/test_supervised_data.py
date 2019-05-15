6504ab9e476b0d5c2f570cbdbe45c667d5224b8b
Samuel Murray <samuelmu@kth.se>
2019-05-15 17:55:46 +0200

    Split test_data.py into test_supervised_data.py and test_unsupervised_data.py

---
+++ tfgp/tests/test_supervised_data.py
@@ -0,0 +1,53 @@
+import numpy as np
+import tensorflow as tf
+
+from tfgp.data import Supervised
+from tfgp.likelihood import MixedLikelihoodWrapper
+
+
+class TestSupervisedData(tf.test.TestCase):
+    def setUp(self) -> None:
+        self.num_data = 10
+
+    def tearDown(self) -> None:
+        tf.reset_default_graph()
+
+    def test_sin(self) -> None:
+        x, likelihood, y = Supervised.make_sin(self.num_data)
+        self.assertIsInstance(x, np.ndarray)
+        self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
+        self.assertIsInstance(y, np.ndarray)
+
+    def test_sin_binary(self) -> None:
+        x, likelihood, y = Supervised.make_sin_binary(self.num_data)
+        self.assertIsInstance(x, np.ndarray)
+        self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
+        self.assertIsInstance(y, np.ndarray)
+
+    def test_sin_count(self) -> None:
+        x, likelihood, y = Supervised.make_sin_count(self.num_data)
+        self.assertIsInstance(x, np.ndarray)
+        self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
+        self.assertIsInstance(y, np.ndarray)
+
+    def test_xcos(self) -> None:
+        x, likelihood, y = Supervised.make_xcos(self.num_data)
+        self.assertIsInstance(x, np.ndarray)
+        self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
+        self.assertIsInstance(y, np.ndarray)
+
+    def test_xcos_binary(self) -> None:
+        x, likelihood, y = Supervised.make_xcos_binary(self.num_data)
+        self.assertIsInstance(x, np.ndarray)
+        self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
+        self.assertIsInstance(y, np.ndarray)
+
+    def test_xsin_count(self) -> None:
+        x, likelihood, y = Supervised.make_xsin_count(self.num_data)
+        self.assertIsInstance(x, np.ndarray)
+        self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
+        self.assertIsInstance(y, np.ndarray)
+
+
+if __name__ == "__main__":
+    tf.test.main()
