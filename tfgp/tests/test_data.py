50058864c3b93b9fb1cd172ad40065c619dfa24f
Samuel Murray <samuelmu@kth.se>
2019-05-15 17:40:16 +0200

    Move data to new package and change functions to static methods

--- tfgp/tests/test_data.py
+++ tfgp/tests/test_data.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import tensorflow as tf
 
-from tfgp.util import data
+from tfgp.data import Supervised, Unsupervised
 from tfgp.likelihood import MixedLikelihoodWrapper
 
 
@@ -15,37 +15,37 @@ class TestSupervisedData(tf.test.TestCase):
         tf.reset_default_graph()
 
     def test_sin(self) -> None:
-        x, likelihood, y = data.make_sin(self.num_data)
+        x, likelihood, y = Supervised.make_sin(self.num_data)
         self.assertIsInstance(x, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(y, np.ndarray)
 
     def test_sin_binary(self) -> None:
-        x, likelihood, y = data.make_sin_binary(self.num_data)
+        x, likelihood, y = Supervised.make_sin_binary(self.num_data)
         self.assertIsInstance(x, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(y, np.ndarray)
 
     def test_sin_count(self) -> None:
-        x, likelihood, y = data.make_sin_count(self.num_data)
+        x, likelihood, y = Supervised.make_sin_count(self.num_data)
         self.assertIsInstance(x, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(y, np.ndarray)
 
     def test_xcos(self) -> None:
-        x, likelihood, y = data.make_xcos(self.num_data)
+        x, likelihood, y = Supervised.make_xcos(self.num_data)
         self.assertIsInstance(x, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(y, np.ndarray)
 
     def test_xcos_binary(self) -> None:
-        x, likelihood, y = data.make_xcos_binary(self.num_data)
+        x, likelihood, y = Supervised.make_xcos_binary(self.num_data)
         self.assertIsInstance(x, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(y, np.ndarray)
 
     def test_xsin_count(self) -> None:
-        x, likelihood, y = data.make_xsin_count(self.num_data)
+        x, likelihood, y = Supervised.make_xsin_count(self.num_data)
         self.assertIsInstance(x, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(y, np.ndarray)
@@ -59,25 +59,25 @@ class TestUnsupervisedData(tf.test.TestCase):
         tf.reset_default_graph()
 
     def test_circle(self) -> None:
-        y, likelihood, labels = data.make_circle(self.num_data, 2)
+        y, likelihood, labels = Unsupervised.make_circle(self.num_data, 2)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_circle_non_gaussian(self) -> None:
-        y, likelihood, labels = data.make_circle(self.num_data, 2, gaussian=False)
+        y, likelihood, labels = Unsupervised.make_circle(self.num_data, 2, gaussian=False)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_gaussian_blobs(self) -> None:
-        y, likelihood, labels = data.make_gaussian_blobs(self.num_data, 2, 2)
+        y, likelihood, labels = Unsupervised.make_gaussian_blobs(self.num_data, 2, 2)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_normal_binary(self) -> None:
-        y, likelihood, labels = data.make_normal_binary(self.num_data)
+        y, likelihood, labels = Unsupervised.make_normal_binary(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
@@ -97,79 +97,79 @@ class TestStubOutDataFilesExist(tf.test.TestCase):
         tf.reset_default_graph()
 
     def test_abalone(self) -> None:
-        y, likelihood, labels = data.make_abalone(self.num_data)
+        y, likelihood, labels = Unsupervised.make_abalone(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_adult(self) -> None:
-        y, likelihood, labels = data.make_adult(self.num_data)
+        y, likelihood, labels = Unsupervised.make_adult(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_atr(self) -> None:
-        y, likelihood, labels = data.make_atr(self.num_data)
+        y, likelihood, labels = Unsupervised.make_atr(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_binaryalphadigits(self) -> None:
-        y, likelihood, labels = data.make_binaryalphadigits(self.num_data)
+        y, likelihood, labels = Unsupervised.make_binaryalphadigits(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_binaryalphadigits_test(self) -> None:
-        y, likelihood, labels = data.make_binaryalphadigits_test(self.num_data)
+        y, likelihood, labels = Unsupervised.make_binaryalphadigits_test(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_cleveland(self) -> None:
-        y, likelihood, labels = data.make_cleveland(self.num_data)
+        y, likelihood, labels = Unsupervised.make_cleveland(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_cleveland_quantized(self) -> None:
-        y, likelihood, labels = data.make_cleveland_quantized(self.num_data)
+        y, likelihood, labels = Unsupervised.make_cleveland_quantized(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_default_credit(self) -> None:
-        y, likelihood, labels = data.make_default_credit(self.num_data)
+        y, likelihood, labels = Unsupervised.make_default_credit(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_mimic(self) -> None:
-        y, likelihood, labels = data.make_mimic(self.num_data)
+        y, likelihood, labels = Unsupervised.make_mimic(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_mimic_labeled(self) -> None:
-        y, likelihood, labels = data.make_mimic_labeled(self.num_data)
+        y, likelihood, labels = Unsupervised.make_mimic_labeled(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_mimic_test(self) -> None:
-        y, likelihood, labels = data.make_mimic_test(self.num_data)
+        y, likelihood, labels = Unsupervised.make_mimic_test(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_wine(self) -> None:
-        y, likelihood, labels = data.make_wine(self.num_data)
+        y, likelihood, labels = Unsupervised.make_wine(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
 
     def test_wine_pos(self) -> None:
-        y, likelihood, labels = data.make_wine_pos(self.num_data)
+        y, likelihood, labels = Unsupervised.make_wine_pos(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
@@ -187,55 +187,55 @@ class TestStubOutFileNotFound(tf.test.TestCase):
 
     def test_abalone(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_abalone(self.num_data)
+            Unsupervised.make_abalone(self.num_data)
 
     def test_adult(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_adult(self.num_data)
+            Unsupervised.make_adult(self.num_data)
 
     def test_atr(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_atr(self.num_data)
+            Unsupervised.make_atr(self.num_data)
 
     def test_binaryalphadigits(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_binaryalphadigits(self.num_data)
+            Unsupervised.make_binaryalphadigits(self.num_data)
 
     def test_binaryalphadigits_test(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_binaryalphadigits_test(self.num_data)
+            Unsupervised.make_binaryalphadigits_test(self.num_data)
 
     def test_cleveland(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_cleveland(self.num_data)
+            Unsupervised.make_cleveland(self.num_data)
 
     def test_cleveland_quantized(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_cleveland_quantized(self.num_data)
+            Unsupervised.make_cleveland_quantized(self.num_data)
 
     def test_default_credit(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_default_credit(self.num_data)
+            Unsupervised.make_default_credit(self.num_data)
 
     def test_mimic(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_mimic(self.num_data)
+            Unsupervised.make_mimic(self.num_data)
 
     def test_mimic_labeled(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_mimic_labeled(self.num_data)
+            Unsupervised.make_mimic_labeled(self.num_data)
 
     def test_mimic_test(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_mimic_test(self.num_data)
+            Unsupervised.make_mimic_test(self.num_data)
 
     def test_wine(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_wine(self.num_data)
+            Unsupervised.make_wine(self.num_data)
 
     def test_wine_pos(self) -> None:
         with self.assertRaises(FileNotFoundError):
-            data.make_wine_pos(self.num_data)
+            Unsupervised.make_wine_pos(self.num_data)
 
 
 class TestMockPodsPackage(tf.test.TestCase):
@@ -245,13 +245,13 @@ class TestMockPodsPackage(tf.test.TestCase):
     def tearDown(self) -> None:
         tf.reset_default_graph()
 
-    @tf.test.mock.patch("tfgp.util.data.pods")
+    @tf.test.mock.patch("tfgp.data.unsupervised.pods")
     def test_oilflow(self, mocked_pods: tf.test.mock.MagicMock) -> None:
         X = np.empty([10, 12])
         Y = np.empty([10, 3])
         mocked_data = {"X": X, "Y": Y}
         mocked_pods.datasets.oil = lambda: mocked_data
-        y, likelihood, labels = data.make_oilflow(self.num_data)
+        y, likelihood, labels = Unsupervised.make_oilflow(self.num_data)
         self.assertIsInstance(y, np.ndarray)
         self.assertIsInstance(likelihood, MixedLikelihoodWrapper)
         self.assertIsInstance(labels, np.ndarray)
