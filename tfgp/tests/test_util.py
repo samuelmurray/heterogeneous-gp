b455c197483c6f63d8e4d58dac6ff89cbd0609ec
Samuel Murray <samuelmu@kth.se>
2019-05-16 17:04:32 +0200

    Initial commit

---
+++ tfgp/tests/test_util.py
@@ -0,0 +1,30 @@
+import numpy as np
+import tensorflow as tf
+
+from tfgp import util
+
+
+class TestUtil(tf.test.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        tf.reset_default_graph()
+
+    def test_categorical_error(self) -> None:
+        prediction = np.array([[1., 0.], [1., 0.], [0., 1.], [0., 1.]])
+        nans = np.ones_like(prediction) * np.nan
+        ground_truth = np.array([[1., 0.], [0., 1.], [1., 0.], [0., 1.]])
+        error = util.categorical_error(prediction, nans, ground_truth)
+        self.assertEqual(0.5, error)
+
+    def test_categorical_error_nans(self) -> None:
+        prediction = np.array([[1., 0.], [1., 0.], [0., 1.], [0., 1.]])
+        nans = np.array([[np.nan, np.nan], [np.nan, np.nan], [np.nan, np.nan], [0., 1.]])
+        ground_truth = np.array([[1., 0.], [0., 1.], [1., 0.], [0., 1.]])
+        error = util.categorical_error(prediction, nans, ground_truth)
+        self.assertAlmostEqual(2 / 3, error)
+
+
+if __name__ == "__main__":
+    tf.test.main()
