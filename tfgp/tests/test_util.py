41c09d96b14726338436fa24021b0f28a34b3849
Samuel Murray <samuelmu@kth.se>
2019-05-17 11:40:28 +0200

    Add tests for range_normalised_rmse

--- tfgp/tests/test_util.py
+++ tfgp/tests/test_util.py
@@ -39,6 +39,20 @@ class TestUtil(tf.test.TestCase):
         error = util.ordinal_error(prediction, nans, ground_truth)
         self.assertAlmostEqual(1 / 9, error)
 
+    def test_range_normalised_rmse(self) -> None:
+        prediction = np.array([[5.], [4.], [3.]])
+        nans = np.array([[np.nan], [np.nan], [np.nan]])
+        ground_truth = np.array([[4.], [2.], [0.]])
+        error = util.range_normalised_rmse(prediction, nans, ground_truth)
+        self.assertAlmostEqual(np.sqrt(7/24), error)
+
+    def test_range_normalised_rmse_nans(self) -> None:
+        prediction = np.array([[5.], [4.], [3.]])
+        nans = np.array([[np.nan], [np.nan], [0.]])
+        ground_truth = np.array([[4.], [2.], [0.]])
+        error = util.range_normalised_rmse(prediction, nans, ground_truth)
+        self.assertAlmostEqual(np.sqrt(5/8), error)
+
 
 if __name__ == "__main__":
     tf.test.main()
