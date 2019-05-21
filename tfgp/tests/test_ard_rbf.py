7342307f533e9ea0fa59704948c6d4da58d735af
Samuel Murray <samuelmu@kth.se>
2019-05-21 13:03:06 +0200

    Extract variable x_dim

--- tfgp/tests/test_ard_rbf.py
+++ tfgp/tests/test_ard_rbf.py
@@ -9,21 +9,22 @@ class TestARDRBF(tf.test.TestCase):
     def setUp(self) -> None:
         np.random.seed(1363431413)
         tf.random.set_random_seed(1534135313)
+        self.x_dim = 5
         with tf.variable_scope("ardrbf", reuse=tf.AUTO_REUSE):
-            self.kernel = ARDRBF(1., 0.5, x_dim=5)
+            self.kernel = ARDRBF(1., 0.5, x_dim=self.x_dim)
 
     def tearDown(self) -> None:
         tf.reset_default_graph()
 
     def test_throw(self) -> None:
-        a = tf.convert_to_tensor(np.random.normal(size=(10, 5)), dtype=tf.float32)
-        b = tf.convert_to_tensor(np.random.normal(size=(12, 4)), dtype=tf.float32)
+        a = tf.convert_to_tensor(np.random.normal(size=(10, self.x_dim)), dtype=tf.float32)
+        b = tf.convert_to_tensor(np.random.normal(size=(12, self.x_dim - 1)), dtype=tf.float32)
         with self.assertRaises(ValueError):
             self.kernel(a, b)
 
     def test_equal_to_sklearn(self) -> None:
-        a = np.random.normal(size=(7, 5))
-        b = np.random.normal(size=(6, 5))
+        a = np.random.normal(size=(7, self.x_dim))
+        b = np.random.normal(size=(6, self.x_dim))
         k_sklearn = rbf_kernel(a, b, gamma=0.5)
         k_ard_rbf = self.kernel(tf.convert_to_tensor(a, dtype=tf.float32),
                                 tf.convert_to_tensor(b, dtype=tf.float32))
@@ -34,18 +35,18 @@ class TestARDRBF(tf.test.TestCase):
         self.assertAllClose(k_ab, k_sklearn)
 
     def test_simple(self) -> None:
-        a = tf.convert_to_tensor(np.random.normal(size=(10, 5)), dtype=tf.float32)
+        a = tf.convert_to_tensor(np.random.normal(size=(10, self.x_dim)), dtype=tf.float32)
         k = self.kernel(a)
         self.assertShapeEqual(np.empty([10, 10]), k)
 
     def test_extended(self) -> None:
-        a = tf.convert_to_tensor(np.random.normal(size=(2, 10, 5)), dtype=tf.float32)
+        a = tf.convert_to_tensor(np.random.normal(size=(2, 10, self.x_dim)), dtype=tf.float32)
         k = self.kernel(a)
         self.assertShapeEqual(np.empty([2, 10, 10]), k)
 
     def test_full(self) -> None:
-        a = tf.convert_to_tensor(np.random.normal(size=(2, 10, 5)), dtype=tf.float32)
-        b = tf.convert_to_tensor(np.random.normal(size=(2, 12, 5)), dtype=tf.float32)
+        a = tf.convert_to_tensor(np.random.normal(size=(2, 10, self.x_dim)), dtype=tf.float32)
+        b = tf.convert_to_tensor(np.random.normal(size=(2, 12, self.x_dim)), dtype=tf.float32)
         k = self.kernel(a, b)
         self.assertShapeEqual(np.empty([2, 10, 12]), k)
 
