ef93bd571b987eea9b8f3f4eeac0186e4675a05b
Samuel Murray <samuelmu@kth.se>
2019-06-24 12:40:27 +0200

    Bug fix

--- hgp/tests/test_mlgplvm.py
+++ hgp/tests/test_mlgplvm.py
@@ -57,8 +57,8 @@ class TestMLGPLVM(tf.test.TestCase):
             _ = MLGPLVM(y, self.latent_dim + 1, x=x, kernel=kernel, likelihood=likelihood)
 
     def test_specifying_x(self) -> None:
-        x = np.empty((self.num_data, self.latent_dim))
-        y = np.empty((self.num_data, self.output_dim))
+        x = np.random.randn(self.num_data, self.latent_dim)
+        y = np.random.randn(self.num_data, self.output_dim)
         kernel = RBF()
         likelihood = LikelihoodWrapper([Normal() for _ in range(self.output_dim)])
         m = MLGPLVM(y, self.latent_dim, x=x, kernel=kernel, likelihood=likelihood)
