d808ff42ba59a50452769b4202f7673ac6140219
Samuel Murray <samuelmu@kth.se>
2020-08-05 21:31:31 +0200

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,100 @@
+# ADULT
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 60 --missing_randomly --epochs 2000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 70 --missing_randomly --epochs 2000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 80 --missing_randomly --epochs 2000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 90 --missing_randomly --epochs 2000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 60 --missing_randomly --epochs 2000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 70 --missing_randomly --epochs 2000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 80 --missing_randomly --epochs 2000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 90 --missing_randomly --epochs 2000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 60 --missing_randomly --epochs 2000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 70 --missing_randomly --epochs 2000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 80 --missing_randomly --epochs 2000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 90 --missing_randomly --epochs 2000 --subsample_data 10
+
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 60 --missing_randomly --epochs 10000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 70 --missing_randomly --epochs 10000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 80 --missing_randomly --epochs 10000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 90 --missing_randomly --epochs 10000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 60 --missing_randomly --epochs 10000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 70 --missing_randomly --epochs 10000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 80 --missing_randomly --epochs 10000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 90 --missing_randomly --epochs 10000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 60 --missing_randomly --epochs 10000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 70 --missing_randomly --epochs 10000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 80 --missing_randomly --epochs 10000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 90 --missing_randomly --epochs 10000 --subsample_data 10
+
+# WINE
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 10 --epochs 20000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 20 --epochs 20000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 30 --epochs 20000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 40 --epochs 20000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 50 --epochs 20000 --subsample_data 10
+
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 10 --epochs 20000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 20 --epochs 20000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 30 --epochs 20000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 40 --epochs 20000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 50 --epochs 20000 --subsample_data 10
+
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 60 --missing_randomly --epochs 2000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 70 --missing_randomly --epochs 2000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 80 --missing_randomly --epochs 2000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 90 --missing_randomly --epochs 2000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 60 --missing_randomly --epochs 2000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 70 --missing_randomly --epochs 2000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 80 --missing_randomly --epochs 2000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 90 --missing_randomly --epochs 2000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 60 --missing_randomly --epochs 2000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 70 --missing_randomly --epochs 2000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 80 --missing_randomly --epochs 2000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data adult --missing 90 --missing_randomly --epochs 2000 --subsample_data 10
+
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 60 --missing_randomly --epochs 10000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 70 --missing_randomly --epochs 10000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 80 --missing_randomly --epochs 10000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 90 --missing_randomly --epochs 10000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 60 --missing_randomly --epochs 10000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 70 --missing_randomly --epochs 10000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 80 --missing_randomly --epochs 10000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 90 --missing_randomly --epochs 10000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 60 --missing_randomly --epochs 10000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 70 --missing_randomly --epochs 10000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 80 --missing_randomly --epochs 10000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data adult --missing 90 --missing_randomly --epochs 10000 --subsample_data 10
+
+# DEFAULT CREDIT
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 60 --missing_randomly --epochs 1000 --subsample_data 100
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 70 --missing_randomly --epochs 1000 --subsample_data 100
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 80 --missing_randomly --epochs 1000 --subsample_data 100
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 90 --missing_randomly --epochs 1000 --subsample_data 100
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 60 --missing_randomly --epochs 2000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 70 --missing_randomly --epochs 2000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 80 --missing_randomly --epochs 2000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 90 --missing_randomly --epochs 2000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 60 --missing_randomly --epochs 2000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 70 --missing_randomly --epochs 2000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 80 --missing_randomly --epochs 2000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 90 --missing_randomly --epochs 2000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 60 --missing_randomly --epochs 2000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 70 --missing_randomly --epochs 2000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 80 --missing_randomly --epochs 2000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model batch --data default_credit --missing 90 --missing_randomly --epochs 2000 --subsample_data 10
+
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 60 --missing_randomly --epochs 5000 --subsample_data 100
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 70 --missing_randomly --epochs 5000 --subsample_data 100
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 80 --missing_randomly --epochs 5000 --subsample_data 100
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 90 --missing_randomly --epochs 5000 --subsample_data 100
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 60 --missing_randomly --epochs 10000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 70 --missing_randomly --epochs 10000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 80 --missing_randomly --epochs 10000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 90 --missing_randomly --epochs 10000 --subsample_data 50
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 60 --missing_randomly --epochs 10000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 70 --missing_randomly --epochs 10000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 80 --missing_randomly --epochs 10000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 90 --missing_randomly --epochs 10000 --subsample_data 20
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 60 --missing_randomly --epochs 10000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 70 --missing_randomly --epochs 10000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 80 --missing_randomly --epochs 10000 --subsample_data 10
+pipenv run python hgp/examples/run_impute.py --model vae --data default_credit --missing 90 --missing_randomly --epochs 10000 --subsample_data 10
